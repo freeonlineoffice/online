35673e7d18406749153743b6b053177d420b08c3
NickWingate <nick.wingate@collabora.com>
2022-05-12 09:10:39 +0100

    Expose object name and description dialogs
    It can be used to apply name to an object so it will
    be presented in the navigator as something unique
    
    Signed-off-by: NickWingate <nick.wingate@collabora.com>
    Change-Id: Ica06259dc3007d638a4998ccc2cce03ad1a5ca05

--- browser/src/control/Control.Menubar.js
+++ browser/src/control/Control.Menubar.js
@@ -277,7 +277,9 @@ L.Control.Menubar = L.Control.extend({
 				{type: 'separator'},
 				{uno: '.uno:TransformDialog'},
 				{uno: '.uno:FormatLine'},
-				{uno: '.uno:FormatArea'}
+				{uno: '.uno:FormatArea'},
+				{uno: '.uno:NameGroup'},
+				{uno: '.uno:ObjectTitleDescription'},
 			]},
 			{name: _('References'), id: 'references', type: 'menu', menu: [
 				{name: _UNO('.uno:IndexesMenu', 'text'), uno: '.uno:InsertMultiIndex'},
@@ -474,6 +476,8 @@ L.Control.Menubar = L.Control.extend({
 				{uno: '.uno:TransformDialog'},
 				{uno: '.uno:FormatLine'},
 				{uno: '.uno:FormatArea'},
+				{uno: '.uno:NameGroup'},
+				{uno: '.uno:ObjectTitleDescription'},
 				{type: 'separator'},
 				{uno: '.uno:OutlineBullet'},
 				{uno: '.uno:ThemeDialog'}]
@@ -610,6 +614,8 @@ L.Control.Menubar = L.Control.extend({
 				{uno: '.uno:TransformDialog'},
 				{uno: '.uno:FormatLine'},
 				{uno: '.uno:FormatArea'},
+				{uno: '.uno:NameGroup'},
+				{uno: '.uno:ObjectTitleDescription'},
 				{type: 'separator'},
 				{uno: '.uno:OutlineBullet'},
 				{uno: '.uno:ThemeDialog'}]
