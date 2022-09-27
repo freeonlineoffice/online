79e9c44c7db233970c36d4147d097afb14dbe454
Tor Lillqvist <tml@collabora.com>
2022-09-27 11:40:25 +0300

    Add alternative ways to handle "missing fonts": log, report, or ignore
    The default is to log to the coolwsd log file.
    
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: I894f8c4b19ee96f7323512e63c8c66372d8516d9

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -2284,6 +2284,10 @@ void LOOLWSD::innerInitialize(Application& self)
     // Initialize the config subsystem too.
     config::initialize(&config());
 
+    // For some reason I can't get at this setting in ChildSession::loKitCallback().
+    std::string fontsMissingHandling = config::getString("fonts_missing.handling", "log");
+    setenv("FONTS_MISSING_HANDLING", fontsMissingHandling.c_str(), 1);
+
     IsBindMountingEnabled = getConfigValue<bool>(conf, "mount_jail_tree", true);
 #if CODE_COVERAGE
     // Code coverage is not supported with bind-mounting.
