abee218c0fe0617aaa52b3faef92eb16d9e95c95
andreasma <maand@gmx.de>
2022-10-11 20:58:44 +0200

    fix module un-def new2
    Signed-off-by: andreasma <maand@gmx.de>
    Change-Id: I01b7bb90526893c62548905d0b3dea7ec96befb5

--- browser/.eslintrc.js
+++ browser/.eslintrc.js
@@ -1,10 +1,13 @@
-/*global module */
-/*eslint no-undef: "error"*/
 module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
+    "globals": {
+        "L": true,
+        "module": false,
+        "define": false
+      },
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended"
