6bbabe18001861e25ff5df356411cc20f815b7b0
Tor Lillqvist <tml@collabora.com>
2022-11-08 15:34:02 +0200

    RequestDetails::_isMobile is unused
    Signed-off-by: Tor Lillqvist <tml@collabora.com>
    Change-Id: Ib2a6bb69b09776dbaf48216330f1c260299d9cac

--- wsd/RequestDetails.cpp
+++ wsd/RequestDetails.cpp
@@ -68,7 +68,6 @@ template <typename T> bool equal(const T& lhs, const T& rhs)
 }
 
 RequestDetails::RequestDetails(Poco::Net::HTTPRequest &request, const std::string& serviceRoot)
-    : _isMobile(false)
 {
     // Check and remove the ServiceRoot from the request.getURI()
     if (!Util::startsWith(request.getURI(), serviceRoot))
@@ -102,7 +101,6 @@ RequestDetails::RequestDetails(const std::string &mobileURI)
     , _isProxy(false)
     , _isWebSocket(false)
 {
-    _isMobile = true;
     _uriString = mobileURI;
     dehexify();
     processURI();
