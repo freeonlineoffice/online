e4a1e9c0152dd2e918f0caf8a371430bd79cedfb
andreasma <maand@gmx.de>
2022-11-15 20:22:12 +0100

    wsd: test: refactor client connection management
    Adapt commit 5892bcba1ea04de69aa3f530cdb1d4239a096507 from Ashod
    Nakashian to LibreOffice Online.
    
    The client connection management had nothing to
    do with the WopiTestServer and it was there
    for convenience. The issue was that it was
    not available to tests that do not need
    the custom WopiTestServer and depending
    on it would make them unnecessarily
    complicated.
    
    This adds a new intermediary class to manage
    client connections in tests. The reason
    this logic didn't move to UnitWSD or even
    UnitBase is simply because the client
    connection logic depends on the helpers
    namespace and other test-specific code
    that isn't available in LOOLWSD. In short,
    UnitBase and UnitWSD are primarily an
    interface that LOOLWSD and co depend on
    and cannot contain test-specific logic
    that isn't linked to coolwsd.
    
    We also move the UnitWebSocket helper class.
    
    Signed-off-by: andreasma <maand@gmx.de>
    Change-Id: I678bbe6166d542480f77b8c709361482ee8f3114

--- test/WopiTestServer.hpp
+++ test/WopiTestServer.hpp
@@ -13,8 +13,8 @@
 #include "helpers.hpp"
 #include "Log.hpp"
 #include "Unit.hpp"
-#include "UnitHTTP.hpp"
 #include "Util.hpp"
+#include "UnitWSDClient.hpp"
 
 #include <Poco/JSON/Object.h>
 #include <Poco/URI.h>
@@ -37,7 +37,7 @@ class HTTPRequest;
 /// Furthermore, the file URI doesn't contain the
 /// real filename (in most tests), instead filenames
 /// 1 to 9 are considered special.
-class WopiTestServer : public UnitWSD
+class WopiTestServer : public UnitWSDClient
 {
 private:
 
@@ -45,15 +45,6 @@ private:
     {
         DocChanged = 1010
     };
-
-    /// The WOPISrc URL.
-    std::string _wopiSrc;
-
-    /// The SocketPoll thread.
-    std::shared_ptr<SocketPoll> _socketPoll;
-
-    /// Websockets to communicate.
-    std::vector< std::unique_ptr<UnitWebSocket> > _wsList;
 
     /// Content of the file.
     std::string _fileContent;
@@ -106,8 +97,7 @@ protected:
     }
 
     WopiTestServer(const std::string& name, const std::string& filenameOrContents = "Hello, world")
-        : UnitWSD(name)
-        , _socketPoll(std::make_shared<SocketPoll>(name + "ServerPoll"))
+        : UnitWSDClient(name)
         , _filename(DefaultFilename)
         , _countCheckFileInfo(0)
         , _countGetFile(0)
@@ -115,8 +105,6 @@ protected:
         , _countPutFile(0)
     {
         LOG_TST("WopiTestServer created for [" << getTestname() << ']');
-
-        _socketPoll->startThread();
 
         // Read the document data and store as string in memory.
         const auto data = helpers::readDataFromFile(filenameOrContents);
@@ -145,45 +133,6 @@ protected:
     void resetCountPutRelative() { _countPutRelative = 0; }
     std::size_t getCountPutFile() const { return _countPutFile; }
     void resetCountPutFile() { _countPutFile = 0; }
-
-    void initWebsocket(const std::string& wopiName)
-    {
-        Poco::URI wopiURL(helpers::getTestServerURI() + wopiName + "&testname=" + getTestname());
-
-        _wopiSrc.clear();
-        Poco::URI::encode(wopiURL.toString(), ":/?", _wopiSrc);
-
-        // This is just a client connection that is used from the tests.
-        // It really has nothing to do with this fake WOPI server, exept
-        // that it manages it since it is the base of WOPI tests, so
-        // it's a common bit of housekeeping that all WOPI tests must do.
-        LOG_TST("Connecting test client to LOOL (#" << (_wsList.size() + 1)
-                                                    << " connection): /lool/" << _wopiSrc << "/ws");
-
-        // Insert at the front.
-        const auto& _ws = _wsList.emplace(
-            _wsList.begin(),
-            Util::make_unique<UnitWebSocket>(_socketPoll, "/lool/" + _wopiSrc + "/ws"));
-
-        assert((*_ws).get());
-    }
-
-    void addWebSocket()
-    {
-        // This is just a client connection that is used from the tests.
-        // It really has nothing to do with this fake WOPI server, exept
-        // that it manages it since it is the base of WOPI tests, so
-        // it's a common bit of housekeeping that all WOPI tests must do.
-        LOG_TST("Connecting test client to LOOL (#" << (_wsList.size() + 1)
-                                                    << " connection): /lool/" << _wopiSrc << "/ws");
-
-        // Insert at the back.
-        const auto& _ws = _wsList.emplace(
-            _wsList.end(),
-            Util::make_unique<UnitWebSocket>(_socketPoll, "/lool/" + _wopiSrc + "/ws"));
-
-        assert((*_ws).get());
-    }
 
     virtual void assertCheckFileInfoRequest(const Poco::Net::HTTPRequest& /*request*/)
     {
@@ -513,16 +462,5 @@ protected:
 
 };
 
-/// Send a command message to WSD from a WopiTestServer on the given connection.
-#define WSD_CMD_BY_CONNECTION_INDEX(INDEX, MSG)                                                    \
-    do                                                                                             \
-    {                                                                                              \
-        LOG_TST("Sending from #" << INDEX << ": " << MSG);                                         \
-        helpers::sendTextFrame(getWsAt(INDEX)->getWebSocket(), MSG, getTestname());                \
-        SocketPoll::wakeupWorld();                                                                 \
-    } while (false)
-
-/// Send a command message to WSD from a WopiTestServer on the primary connection.
-#define WSD_CMD(MSG) WSD_CMD_BY_CONNECTION_INDEX(0, MSG)
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
