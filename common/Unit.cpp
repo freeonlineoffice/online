f5a5d57bdcf01a98468bfca677fb385efb30ce15
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2022-11-24 08:49:47 -0500

    wsd: test: wakeup world when starting a new test
    This is needed so we stop the DocBroker of
    the old test, which is now finished.
    
    Change-Id: I05df85fbbeca254928cd9a140198e7f4ae89fec8
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- common/Unit.cpp
+++ common/Unit.cpp
@@ -508,6 +508,9 @@ void UnitBase::exitTest(TestResult result, const std::string& reason)
             if (GlobalWSD)
                 GlobalWSD->configure(Poco::Util::Application::instance().config());
             GlobalArray[GlobalIndex]->initialize();
+
+            // Wake-up so the previous test stops.
+            SocketPoll::wakeupWorld();
             return;
         }
     }
