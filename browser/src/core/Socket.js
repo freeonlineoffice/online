2f591c6a2fefd10cad4001b35851f7744f9beb9d
andreasma <maand@gmx.de>
2023-01-16 19:44:43 +0100

    Fix code
    Signed-off-by: andreasma <maand@gmx.de>
    Change-Id: Ifd3a1372af54a3f7634e34265e095053a647b42a

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -1103,12 +1103,10 @@ app.definitions.Socket = L.Class.extend({
 				textMsg = textMsg.replace(/{connections}/g, command.params[1]);
 				textMsg = textMsg.replace(/{productname}/g, (typeof brandProductName !== 'undefined' ?
 					brandProductName : 'Free Online Office'));
-				var brandFAQURL = (typeof brandProductFAQURL !== 'undefined') ?
-					brandProductFAQURL : 'https://hub.libreoffice.org/professional-online-support';
 				this._map.fire('infobar',
 					{
 						msg: textMsg,
-						action: brandFAQURL,
+						action: L.Util.getProduct(),
 						actionLabel: errorMessages.infoandsupport
 					});
 			}
