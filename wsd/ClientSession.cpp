31e6ab0236fa979ae8782a709059568aba817d57
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-02-20 20:18:12 -0500

    wsd: handle errors from locking/unlocking a document
    Change-Id: I69e2c26aaa572e5ca215fd8cf3413ed043861f0f
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -1400,6 +1400,8 @@ void ClientSession::sendFileMode(const bool readOnly, const bool editComments)
 
 void ClientSession::setLockFailed(const std::string& sReason)
 {
+    // TODO: make this "read-only" a special one with a notification (infobar? balloon tip?)
+    //       and a button to unlock
     _isLockFailed = true;
     setReadOnly(true);
     sendTextFrame("lockfailed:" + sReason);
