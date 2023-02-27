ac1ceadb260eaec10ec17232c0d7bef8383d169c
Szymon Kłos <szymon.klos@collabora.com>
2023-02-27 06:03:47 +0100

    Send BadRequest if parameters missing
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I2acc4cb22d064994f3fca16b92b7d4528b403044

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -4520,6 +4520,10 @@ private:
             else
             {
                 LOG_INF("Missing parameters for conversion request.");
+                http::Response httpResponse(http::StatusCode::BadRequest);
+                httpResponse.set("Content-Length", "0");
+                socket->sendAndShutdown(httpResponse);
+                socket->ignoreInput();
             }
             return;
         }
