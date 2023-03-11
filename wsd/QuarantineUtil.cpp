ab7570a537229a7e2a638f5aa09ac834a2a8fe99
andreasma <maand@gmx.de>
2023-03-12 00:37:17 +0100

    quarantine: cleanup.
    Also handle OOB values of config setting.
    
    Adapt commit d5e95c80ec10308ad9275bafc6c42b543e57b7a4 from Michael Meeks
    to LibreOffice Online.
    
    Signed-off-by: andreasma <maand@gmx.de>
    Change-Id: I3d942617b5ef9ec99257f419620405948843f289

--- wsd/QuarantineUtil.cpp
+++ wsd/QuarantineUtil.cpp
@@ -114,20 +114,20 @@ namespace Quarantine
         }
     }
 
-    void clearOldQuarantineVersions(const std::string& Wopiscr)
+    void clearOldQuarantineVersions(const std::string& docKey)
     {
         if (!isQuarantineEnabled())
             return;
 
-        std::size_t maxVersionCount = LOOLWSD::getConfigValue<std::size_t>("quarantine_files.max_versions_to_maintain", 2);
+        std::size_t maxVersionCount =
+            std::max<size_t>(LOOLWSD::getConfigValue<std::size_t>("quarantine_files.max_versions_to_maintain", 2), 1);
         std::string decoded;
-        Poco::URI::decode(Wopiscr, decoded);
+        Poco::URI::decode(docKey, decoded);
         while (LOOLWSD::QuarantineMap[decoded].size() > maxVersionCount)
         {
             FileUtil::removeFile(LOOLWSD::QuarantineMap[decoded][0]);
             LOOLWSD::QuarantineMap[decoded].erase(LOOLWSD::QuarantineMap[decoded].begin());
         }
-
     }
 
     bool quarantineFile(DocumentBroker* docBroker, const std::string& docName)
