2cba1ce7c05227e411292951edf998a558de896b
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-04-13 23:16:48 -0400

    wsd: shutdown the Kit socket before terminating to flush
    In some rare cases we can't write to the socket
    before we terminate, and the last message is
    lost. This is observed in tests and especially
    when we simulate socket write failure.
    
    Change-Id: I9fb298edfe53de3c7ea0c2f31c02ba370167f96e
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -1554,6 +1554,7 @@ private:
                 }
 
                 session->sendTextFrameAndLogError("error: cmd=load kind=faileddocloading");
+                session->shutdownNormal();
 
                 LOG_FTL("Failed to load the document. Setting TerminationFlag");
                 SigUtil::setTerminationFlag();
