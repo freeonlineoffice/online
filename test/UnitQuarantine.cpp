ac55a55ddbb9774baa13b0c10245802b6d667b83
andreasma <maand@gmx.de>
2023-05-15 20:35:42 +0200

    Name fix
    Signed-off-by: andreasma <maand@gmx.de>
    Change-Id: Ibece0df82092cb7f28002447340e6ef6745f4d37

--- test/UnitQuarantine.cpp
+++ test/UnitQuarantine.cpp
@@ -96,7 +96,7 @@ public:
 
         assertPutFileCount();
 
-        const std::string wopiTimestamp = request.get("X-COOL-WOPI-Timestamp", std::string());
+        const std::string wopiTimestamp = request.get("X-LOOL-WOPI-Timestamp", std::string());
         const bool force = wopiTimestamp.empty(); // Without a timestamp we force to always store.
 
         // We don't expect overwriting by forced uploading.
