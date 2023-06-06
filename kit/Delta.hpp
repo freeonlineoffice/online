6d2ee324fadfa2e5c00a3d310db7918196d02617
Michael Meeks <michael.meeks@collabora.com>
2023-06-06 13:16:35 +0100

    delta: Add unit tests for RLE encoding, and debugging support.
    Change-Id: Ie0b62dbad8af2ab6ff95d6279c69de083c2cd888
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- kit/Delta.hpp
+++ kit/Delta.hpp
@@ -62,6 +62,8 @@ struct TileLocation {
 /// A quick and dirty, thread-safe delta generator for last tile changes
 class DeltaGenerator {
 
+    friend class DeltaTests;
+
     // fast - and deltas take lots of size off.
     static const int compressionLevel = -3;
 
@@ -113,7 +115,7 @@ class DeltaGenerator {
                 free(_rleData);
         }
 
-        void initRow(uint32_t *from, unsigned int width)
+        void initRow(const uint32_t *from, unsigned int width)
         {
             uint32_t scratch[width];
 
