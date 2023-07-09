addba778c0000948d70ef9357f7ae4b233630b1a
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-07-09 16:42:30 -0400

    wsd: save and upload rapidly when unloading
    We normally throttle saving and uploading to
    avoid hammering repeatedly, but when unloading,
    we reduce said minimum wait by a factor of 10.
    
    This allows for a quicker unload cycle, esp.
    if we had just saved before unloading, or
    if there was a previous failure.
    
    Change-Id: I8710c9b86c05254ad9b9735d8496dd96900221e2
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -1610,7 +1610,8 @@ void DocumentBroker::uploadToStorage(const std::shared_ptr<ClientSession>& sessi
     LOG_TRC("uploadToStorage [" << session->getId() << "]: " << (force ? "" : "not") << " forced");
 
     // Upload immediately if forced or had no failures. Otherwise, throttle (on failure).
-    if (force || _storageManager.lastUploadSuccessful() || _storageManager.canUploadNow())
+    if (force || _storageManager.lastUploadSuccessful() ||
+        _storageManager.canUploadNow(isUnloading()))
     {
         constexpr bool isRename = false;
         constexpr bool isExport = false;
@@ -2426,7 +2427,7 @@ void DocumentBroker::autoSaveAndStop(const std::string& reason)
         LOOLWSD::getConfigValue<int>("per_document.min_time_between_saves_ms", 500));
 
     // Don't hammer on saving.
-    if (!canStop && _saveManager.canSaveNow())
+    if (!canStop && _saveManager.canSaveNow(isUnloading()))
     {
         // Stop if there is nothing to save.
         const bool possiblyModified = isPossiblyModified();
