f536861e23c73beea50ceba7365ee981090a0f16
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2023-08-14 07:18:41 -0400

    wsd: checkfileinfo_override substitution
    Adapt patch to LibreOffice Online.
    
    Change-Id: I6339cb16917e73cfe45e59e42715e5cbc2ae8bb8
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- test/FileServeWhiteBoxTests.cpp
+++ test/FileServeWhiteBoxTests.cpp
@@ -380,7 +380,8 @@ void FileServeTests::testPreProcessedFileSubstitution()
         { "CSS_VARIABLES",
           "<style>:root {--co-somestyle-text:#123456;--co-somestyle-size:15px;}</style>" },
         { "POSTMESSAGE_ORIGIN", "https://www.example.com:8080" },
-        { "BRANDING_THEME", "cool_brand" }
+        { "BRANDING_THEME", "cool_brand" },
+        { "CHECK_FILE_INFO_OVERRIDE", "DownloadAsPostMessage=true;blah=bleh" }
     };
 
     std::vector<std::string> files;
@@ -410,6 +411,8 @@ void FileServeTests::testPreProcessedFileSubstitution()
                                  variables["POSTMESSAGE_ORIGIN"]);
             Poco::replaceInPlace(orig, std::string("%BRANDING_THEME%"),
                                  variables["BRANDING_THEME"]);
+            Poco::replaceInPlace(orig, std::string("%CHECK_FILE_INFO_OVERRIDE%"),
+                                 variables["CHECK_FILE_INFO_OVERRIDE"]);
 
             LOK_ASSERT_EQUAL(orig, recon);
         }
