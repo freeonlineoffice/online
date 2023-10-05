a334887fa52969a7fb7be48ba51c06702c1e48e2
Rash419 <rashesh.padia@collabora.com>
2023-10-05 12:31:07 +0530

    docker: fix all aliasgroup env variable values added to blocked list
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: Id7480ab88564965cc7aba34c5020daaf18259815

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -3098,8 +3098,9 @@ void LOOLWSD::initializeEnvOptions()
         {
             if (first)
             {
-                _overrideSettings["storage.wopi.alias_groups.group[" + std::to_string(n) +
-                                  "].host"] = alias;
+                const std::string path = "storage.wopi.alias_groups.group[" + std::to_string(n) + "].host";
+                _overrideSettings[path] = alias;
+                _overrideSettings[path + "[@allow]"] = true;
                 first = false;
             }
             else
