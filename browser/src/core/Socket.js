c6e3fff019881fdcf26530b2c23d96edc254e2bf
Rash419 <rashesh.padia@collabora.com>
2023-12-04 15:49:37 +0530

    jfix: tiles not updated after reconnection from idle
    The following scenario was affected:
    - user A and B both goes idle
    - user B starts typing
    - user A reconnects, all tiles updated by user B were not getting
      reflected for user A
    
    Mismatch of canonical ids was causing the issue. On reconnection,
    tilecombine messages were sent before uno:ChangeTheme can update the
    canonical id on server side, therefore the received tile cache from server were
    from old canonical id https://github.com/CollaboraOnline/online/blob/4a8974d107896c7fb4b7c35e326b52a986144a8b/wsd/ClientSession.cpp#L1379
    https://github.com/CollaboraOnline/online/blob/4a8974d107896c7fb4b7c35e326b52a986144a8b/wsd/TileCache.cpp#L482
    
    By reseting the previousTheme on connection we make sure there after
    canonicalidchange we invalidate all the tiles
    
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>
    Change-Id: I874bfe0bd71d176bacf0c7aa768e49613535ebd5
    Signed-off-by: Rash419 <rashesh.padia@collabora.com>

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -1529,6 +1529,7 @@ app.definitions.Socket = L.Class.extend({
 				this._map._isCursorVisible = false;
 
 			this._map._docLayer._resetCanonicalIdStatus();
+			this._map.uiManager.previousTheme = null;
 		}
 
 		if (isActive && this._reconnecting) {
