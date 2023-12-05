5b146264ab67f4b939700f7b189a457695244634
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2023-12-05 14:28:36 +0530

    A small typo in Class name which is cause for `FontColor`
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: Ifa40864a332718f9c58335e6d950149673d03f10
    
    A small typo in Class name which is cause for `FontColor` icon not working in dark mode. it is not fully related here but pushing that as a commit in this PR :)
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: I516220240d204302074f72e20d98df7591cb00cb

--- browser/src/control/Control.NotebookbarImpress.js
+++ browser/src/control/Control.NotebookbarImpress.js
@@ -731,7 +731,7 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 									},
 									{
 										'id': 'home-color',
-										'class': '.unospan-FontColor',
+										'class': 'unospan-FontColor',
 										'type': 'toolitem',
 										'text': _UNO('.uno:Color'),
 										'command': '.uno:Color',
