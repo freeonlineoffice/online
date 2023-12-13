047f57d2a162096e2eb11adacc656f963aa020af
Szymon Kłos <szymon.klos@collabora.com>
2023-12-13 16:46:17 +0100

    annotations: move auto saving properties into CommandSection
    To not depend on external objects to store information.
    Also use more typing.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I410b6679ef5e2a7c4f5e73217d5062864a73bc4c

--- browser/src/layer/tile/CommentListSection.ts
+++ browser/src/layer/tile/CommentListSection.ts
@@ -41,6 +41,8 @@ namespace lool {
 
 export class CommentSection extends CanvasSectionObject {
 	map: any;
+	static autoSavedComment: cool.Comment;
+	static commentWasAutoAdded: boolean;
 
 	// To associate comment id with its index in commentList array.
 	private idIndexMap: Map<any, number>;
@@ -532,7 +534,7 @@ export class CommentSection extends CanvasSectionObject {
 
 			// Object is later removed in onACKComment when newly inserted comment object is available
 			// It's to reduce the flicker when using comment autosave
-			if (!app.view.commentAutoSave)
+			if (!CommentSection.autoSavedComment)
 				this.removeItem(annotation.sectionProperties.data.id);
 		} else if (annotation.sectionProperties.data.trackchange) {
 			comment = {
@@ -1178,7 +1180,7 @@ export class CommentSection extends CanvasSectionObject {
 				if (this.sectionProperties.docLayer._docType === 'spreadsheet')
 					annotation.hide();
 
-				var autoSavedComment = app.view.commentAutoSave;
+				var autoSavedComment = CommentSection.autoSavedComment;
 				if (autoSavedComment) {
 					var isOurComment = annotation.isAutoSaved();
 					if (isOurComment) {
@@ -1189,8 +1191,8 @@ export class CommentSection extends CanvasSectionObject {
 						annotation.edit();
 						if (autoSavedComment.sectionProperties.data.id === 'new')
 							this.removeItem(autoSavedComment.sectionProperties.data.id);
-						app.view.commentAutoSave = null;
-						app.view.commentAutoAdded = true;
+						CommentSection.autoSavedComment = null;
+						CommentSection.commentWasAutoAdded = true;
 					}
 				}
 			}
@@ -1231,8 +1233,8 @@ export class CommentSection extends CanvasSectionObject {
 				modified.update();
 				this.update();
 
-				if (app.view.commentAutoSave) {
-					app.view.commentAutoSave.sectionProperties.autoSave.innerText = _('Autosaved');
+				if (CommentSection.autoSavedComment) {
+					CommentSection.autoSavedComment.sectionProperties.autoSave.innerText = _('Autosaved');
 					if (this.sectionProperties.docLayer._docType === 'spreadsheet')
 						modified.show();
 					modified.edit();
