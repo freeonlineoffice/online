810ff2aaf0da732fe7ca28238df1ab4d074aa709
Gökay Şatır <gokaysatir@gmail.com>
2023-12-18 15:40:56 +0300

    Changes for handling readonly mode click events:
    * These changes are made primarily for reading the hyperlink data from the document in readonly mode.
    * Added readonlyclick event.
    * Added readonlyhyperlinkclicked event.
    * And the handlers for these.
    
    When user clicks on document in readonly mode, a readonlyclick event is sent to the server.
    Childsession checks if there is a hyperlink at the clicked location.
    If so, the information of the hyperlink is sent to the client.
    
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: I9c104a1ec770491725337b9bbb1e2d1c2851321b

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -1115,6 +1115,10 @@ bool ClientSession::_handleInput(const char *buffer, int length)
             return forwardToChild(std::string(buffer, length), docBroker);
         }
     }
+    else if (tokens.equals(0, "readonlyclick"))
+    {
+        return forwardToChild(std::string(buffer, length), docBroker);
+    }
     else if (tokens.equals(0, "attemptlock"))
     {
         return attemptLock(docBroker);
