4a2995a4a1d86dec3af84e2617a8b7422f8e238e
Pranam Lashkari <lpranam@collabora.com>
2023-12-28 12:48:44 +0530

    annotation: calc scroll editing comment with sheet/view
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I1306a45a9308226b197b044c47cadd3e5cc4bf17

--- browser/src/layer/tile/CommentSection.ts
+++ browser/src/layer/tile/CommentSection.ts
@@ -599,6 +599,8 @@ export class Comment extends CanvasSectionObject {
 		this.convertRectanglesToViewCoordinates();
 		this.convertRectanglesToCoreCoordinates();
 		this.setPositionAndSize();
+		if (this.sectionProperties.docLayer._docType === 'spreadsheet')
+			this.positionCalcComment();
 	}
 
 	private updateAnnotationMarker (): void {
