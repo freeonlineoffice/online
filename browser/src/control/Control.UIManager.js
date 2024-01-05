5c908c22d32be033de1c8e44c9a1cee4f36ba80c
Szymon Kłos <szymon.klos@collabora.com>
2024-01-05 15:02:35 +0100

    Check for comments in isAnyDialogOpen
    As described in the comment there was problem with
    userinactive command. This is now solved due to:
    commit fa006fc3afacc7c7369ceaed20833c8c9ba05989
    Don't make view inactive when dialog is opened
    
    Comment was from commit 6550e713c4bf687931aa721f36d0a28ac70e94f0
    annotation: calc scroll vertical to make selected comment visible
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ica947cc743292ce3610f52011396ec5f9a73593c

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -4,7 +4,7 @@
 			 and allows to controll them (show/hide)
  */
 
-/* global app $ setupToolbar w2ui toolbarUpMobileItems _ Hammer JSDialog */
+/* global app lool $ setupToolbar w2ui toolbarUpMobileItems _ Hammer JSDialog */
 L.Control.UIManager = L.Control.extend({
 	mobileWizard: null,
 	documentNameInput: null,
@@ -1133,6 +1133,9 @@ L.Control.UIManager = L.Control.extend({
 	},
 
 	isAnyDialogOpen: function() {
+		if (lool.Comment.isAnyEdit())
+			return true;
+
 		if (this.map.jsdialog)
 			return this.map.jsdialog.hasDialogOpened();
 		else
