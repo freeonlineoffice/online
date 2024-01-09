7d87b15c31d473da021fc978ff163634e526f1c4
Méven Car <meven.car@collabora.com>
2024-01-09 16:30:44 +0100

    context menu: allow edit section for Writer text sections
    Signed-off-by: Méven Car <meven.car@collabora.com>
    Change-Id: I40a40a99551a756ad234e73956169018b761fa42

--- browser/src/control/Control.ContextMenu.js
+++ browser/src/control/Control.ContextMenu.js
@@ -46,7 +46,7 @@ L.Control.ContextMenu = L.Control.extend({
 			text: ['TableInsertMenu',
 				   'InsertRowsBefore', 'InsertRowsAfter', 'InsertColumnsBefore', 'InsertColumnsAfter',
 				   'TableDeleteMenu', 'SetObjectToBackground', 'SetObjectToForeground',
-				   'DeleteRows', 'DeleteColumns', 'DeleteTable',
+				   'DeleteRows', 'DeleteColumns', 'DeleteTable', 'EditCurrentRegion',
 				   'MergeCells', 'SetOptimalColumnWidth', 'SetOptimalRowHeight',
 				   'UpdateCurIndex','RemoveTableOf',
 				   'ReplyComment', 'DeleteComment', 'DeleteAuthor', 'DeleteAllNotes',
