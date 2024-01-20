9d7f79a9ea1b9552dc0a26dd5f95f6ea99f82112
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-01-20 13:02:33 -0500

    wsd: exclude WopiStorage in mobile builds
    This excludes Wopi handling (as much as possible)
    from mobile builds.
    
    Adapt patch to LibreOffice Online.
    
    Change-Id: I4abb00f02e77b5d89d56ced49ce19a37ae93c13e
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/Storage.hpp
+++ wsd/Storage.hpp
@@ -420,7 +420,9 @@ public:
                Unsupported, //< An unsupported type.
                Unauthorized, //< The host is not allowed by the admin.
                FileSystem, //< File-System storage. Only for testing.
+#if !MOBILEAPP
                Wopi //< WOPI-like storage.
+#endif //!MOBILEAPP
     );
 
     /// Validates the given URI.
@@ -554,16 +556,6 @@ private:
 class WopiStorage : public StorageBase
 {
 public:
-    WopiStorage(const Poco::URI& uri, const std::string& localStorePath,
-                const std::string& jailPath)
-        : StorageBase(uri, localStorePath, jailPath)
-        , _wopiSaveDuration(std::chrono::milliseconds::zero())
-    {
-        LOG_INF("WopiStorage ctor with localStorePath: ["
-                << localStorePath << "], jailPath: [" << jailPath << "], uri: ["
-                << LOOLWSD::anonymizeUrl(uri.toString()) << ']');
-    }
-
     class WOPIFileInfo : public FileInfo
     {
         void init();
@@ -693,6 +685,18 @@ public:
         bool _userCanRename = false;
     };
 
+#if !MOBILEAPP
+
+    WopiStorage(const Poco::URI& uri, const std::string& localStorePath,
+                const std::string& jailPath)
+        : StorageBase(uri, localStorePath, jailPath)
+        , _wopiSaveDuration(std::chrono::milliseconds::zero())
+    {
+        LOG_INF("WopiStorage ctor with localStorePath: ["
+                << localStorePath << "], jailPath: [" << jailPath << "], uri: ["
+                << LOOLWSD::anonymizeUrl(uri.toString()) << ']');
+    }
+
     /// Returns the response of CheckFileInfo WOPI call for URI that was
     /// provided during the initial creation of the WOPI storage.
     /// Also extracts the basic file information from the response
@@ -769,6 +773,7 @@ private:
 
     /// The http::Session used for uploading asynchronously.
     std::shared_ptr<http::Session> _uploadHttpSession;
+#endif // !MOBILEAPP
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
