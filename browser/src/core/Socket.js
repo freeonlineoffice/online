8d9b06ea38e48ca91be30b9e6d0a3fd299c12599
Neil Guertin <neil.guertin@collabora.com>
2024-01-26 17:57:43 -0500

    Encapsulate debug overlayData
    Signed-off-by: Neil Guertin <neil.guertin@collabora.com>
    Change-Id: I768562e7e4a17d65338d0f5a64c4346e2f09f53f

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -282,8 +282,9 @@ app.definitions.Socket = L.Class.extend({
 
 		var debugOn = this._map._debug.debugOn;
 
-		if (debugOn)
-			this._map._debug.setOverlayPostMessage(type,msg);
+		if (this._map._debug.overlayOn) {
+			this._map._debug.setOverlayMessage('postMessage',type+': '+msg);
+		}
 
 		if (!debugOn && msg.length > 256) // for reasonable performance.
 			msg = msg.substring(0,256) + '<truncated ' + (msg.length - 256) + 'chars>';
@@ -1065,7 +1066,7 @@ app.definitions.Socket = L.Class.extend({
 			var times = this._map._debug._debugTimePING;
 			var timeText = this._map._debug.updateTimeArray(times, +new Date() - this._map._debug._debugPINGQueue.shift());
 			if (this._map._debug.overlayOn) {
-				this._map._debug.overlayData['ping'].setPrefix(
+				this._map._debug.setOverlayMessage('ping',
 					'Server ping time: ' + timeText + '. ' +
 					'Rendered tiles: ' + command.rendercount + ', ' + 
 					'last: ' + (command.rendercount - this._map._debug._debugRenderCount)
