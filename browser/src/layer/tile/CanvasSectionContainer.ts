0b8df76cf01f09198a3b2a24bab1f3c902e89d11
Méven Car <meven.car@collabora.com>
2024-02-05 10:06:01 +0100

    browser: CanvasSectionContainer initialize boundsList
    Also move its declaration to the top of the class section with other
    class members.
    
    Signed-off-by: Méven Car <meven.car@collabora.com>
    Change-Id: I506423078423074171ab00efb6f2d3fab0c68796

--- browser/src/layer/tile/CanvasSectionContainer.ts
+++ browser/src/layer/tile/CanvasSectionContainer.ts
@@ -219,6 +219,9 @@ class CanvasSectionObject {
 	sectionProperties: any = {};
 	callbacks: SectionCallbacks;
 
+	/// The sections those this section can propagate events to. Updated by container.
+	boundsList: Array<CanvasSectionObject> = [];
+
 	/// Paramaters: null (use sectionProperties).
 	onInitialize(): void {
 		if (this.callbacks.onInitialize)
@@ -369,9 +372,6 @@ class CanvasSectionObject {
 		this.containerObject.updateBoundSectionLists();
 		this.containerObject.reNewAllSections();
 	}
-
-	/// The sections those this section can propagate events to. Updated by container.
-	boundsList: Array<CanvasSectionObject>;
 
 	public stopPropagating(): void {
 		this.containerObject.lowestPropagatedBoundSection = this.name;
