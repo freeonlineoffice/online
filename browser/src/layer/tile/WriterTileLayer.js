64b07d3e2125469702e1b654f756b2aa868d818c
Neil Guertin <neil.guertin@collabora.com>
2024-02-08 17:47:03 -0500

    Minor debug improvements
    Rename and reduce scope of variable associated with
    tile invalidations debug tool
    
    Signed-off-by: Neil Guertin <neil.guertin@collabora.com>
    Change-Id: I3e22d69e9c5867d1194a42520b35231e20b1ca01

--- browser/src/layer/tile/WriterTileLayer.js
+++ browser/src/layer/tile/WriterTileLayer.js
@@ -86,7 +86,7 @@ L.WriterTileLayer = L.CanvasTileLayer.extend({
 		var offset = new L.Point(command.width, command.height);
 		var bottomRightTwips = topLeftTwips.add(offset);
 		if (this._debug.tileInvalidationsOn) {
-			this._debug.addInvalidationRectangle(topLeftTwips, bottomRightTwips, textMsg);
+			this._debug.addTileInvalidationRectangle(topLeftTwips, bottomRightTwips, textMsg);
 		}
 		var invalidBounds = new L.Bounds(topLeftTwips, bottomRightTwips);
 		var visibleTopLeft = this._latLngToTwips(this._map.getBounds().getNorthWest());
@@ -106,7 +106,7 @@ L.WriterTileLayer = L.CanvasTileLayer.extend({
 		}
 
 		if (needsNewTiles && this._debug.tileInvalidationsOn) {
-			this._debug.addInvalidationMessage(textMsg);
+			this._debug.addTileInvalidationMessage(textMsg);
 		}
 
 		this._previewInvalidations.push(invalidBounds);
