21b0137288a7d324d2c3c34e9b07b9b6131d1095
Szymon Kłos <szymon.klos@collabora.com>
2024-03-07 15:58:36 +0100

    remove-w2ui: reuse JSDialog menus for grouped Export buttons
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I1080610bca521c339f4eacba40320e94dbc7b9df

--- browser/src/control/Control.NotebookbarCalc.js
+++ browser/src/control/Control.NotebookbarCalc.js
@@ -110,8 +110,10 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 		if (!this._map['wopi'].UserCanNotWriteRelative) {
 			content.push(
 				(window.uiDefaults && window.uiDefaults.saveAsMode === 'group') ? {
-					'id': 'saveas',
-					'type': 'bigmenubartoolitem',
+					'id': 'saveas:SaveAsMenu',
+					'command': 'saveas',
+					'class': 'unosaveas',
+					'type': 'exportmenubutton',
 					'text': _('Save As'),
 					'accessibility': { focusBack: true,	combination: 'A', de: null }
 				}:
@@ -127,9 +129,10 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 
 		if (!this._map['wopi'].UserCanNotWriteRelative) {
 			content.push({
-				'id': 'exportas',
+				'id': 'exportas:ExportAsMenu',
+				'command': 'exportas',
 				'class': 'unoexportas',
-				'type': 'bigmenubartoolitem',
+				'type': 'exportmenubutton',
 				'text': _('Export As'),
 				'accessibility': { focusBack: true,	combination: 'E', de: null }
 			});
@@ -192,9 +195,10 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 
 		if (!!window.groupDownloadAsForNb && !this._map['wopi'].HideExportOption) {
 			content.push({
-				'id': 'downloadas',
+				'id': 'downloadas:DownloadAsMenu',
+				'command': 'downloadas',
 				'class': 'unodownloadas',
-				'type': 'bigmenubartoolitem',
+				'type': 'exportmenubutton',
 				'text': _('Download'),
 				'accessibility': { focusBack: true,	combination: 'DA', de: null }
 			});
