5ba7a179358107074ea622a51475132f848ba47b
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-03-17 07:45:30 -0400

    killpoco: use http::Response in admin file serving
    Adapt patch to LibreOffice Online.
    
    Change-Id: I9ad49002bc85071770aba08c23e0210bb10ff55b
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -15,6 +15,7 @@
 #include "LOOLWSD.hpp"
 #include "Exceptions.hpp"
 #include "FileUtil.hpp"
+#include "HttpRequest.hpp"
 #include "RequestDetails.hpp"
 #include "ServerURL.hpp"
 #include <Common.hpp>
@@ -262,8 +263,7 @@ bool FileServerRequestHandler::isAdminLoggedIn(const Poco::Net::HTTPRequest& req
 }
 
 bool FileServerRequestHandler::authenticateAdmin(const Poco::Net::HTTPBasicCredentials& credentials,
-                                                 Poco::Net::HTTPResponse& response,
-                                                 std::string& jwtToken)
+                                                 http::Response& response, std::string& jwtToken)
 {
     assert(LOOLWSD::AdminEnabled);
 
@@ -301,35 +301,16 @@ bool FileServerRequestHandler::authenticateAdmin(const Poco::Net::HTTPBasicCrede
     // bundlify appears to add an extra /dist -> dist/dist/admin
     cookie.setPath(LOOLWSD::ServiceRoot + "/browser/dist/");
     cookie.setSecure(LOOLWSD::isSSLEnabled());
-    response.addCookie(cookie);
+    response.header().addCookie(cookie.toString());
 
     return true;
 }
 
-bool FileServerRequestHandler::isAdminLoggedIn(const HTTPRequest& request,
-                                               HTTPResponse &response)
+bool FileServerRequestHandler::isAdminLoggedIn(const HTTPRequest& request, http::Response& response)
 {
     std::string jwtToken;
     return isAdminLoggedIn(request, jwtToken) ||
            authenticateAdmin(Poco::Net::HTTPBasicCredentials(request), response, jwtToken);
-}
-
-bool FileServerRequestHandler::isAdminLoggedIn(const HTTPRequest& request, http::Response& response)
-{
-    // For now, we reuse the exiting implementation, which uses Poco HTTPCookie.
-    Poco::Net::HTTPResponse pocoResponse;
-    if (isAdminLoggedIn(request, pocoResponse))
-    {
-        // Copy the headers, including the cookies.
-        for (const auto& pair : pocoResponse)
-        {
-            response.set(pair.first, pair.second);
-        }
-
-        return true;
-    }
-
-    return false;
 }
 
 #if ENABLE_DEBUG
@@ -1530,7 +1511,7 @@ void FileServerRequestHandler::preprocessAdminFile(const HTTPRequest& request,
     if (!LOOLWSD::AdminEnabled)
         throw Poco::FileAccessDeniedException("Admin console disabled");
 
-    Poco::Net::HTTPResponse response;
+    http::Response response(http::StatusCode::OK);
     std::string jwtToken;
     if (!isAdminLoggedIn(request, jwtToken))
     {
@@ -1610,13 +1591,8 @@ void FileServerRequestHandler::preprocessAdminFile(const HTTPRequest& request,
     response.set("Server", HTTP_SERVER_STRING);
     response.set("Date", Util::getHttpTimeNow());
 
-    response.setContentType("text/html");
-    response.setChunkedTransferEncoding(false);
-
-    std::ostringstream oss;
-    response.write(oss);
-    oss << templateFile;
-    socket->send(oss.str());
+    response.setBody(std::move(templateFile));
+    socket->send(response);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
