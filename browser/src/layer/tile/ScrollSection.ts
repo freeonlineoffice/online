7e4df31a788a9745e7e31166b690553641c8f3e1
Skyler Grey <skyler.grey@collabora.com>
2024-03-18 16:26:03 +0000

    ScrollSection: fix panning with RTL
    Previously we did not take into account whether something was RTL when
    scrolling it with ScrollSection. By introduing a new isRTL function to
    check this, we can do something different (in this case scrolling the
    other direction) if we are controlling an element that should be RTL.
    
    We need to introduce a function, as whether we are using RTL can change
    over the lifetime of the ScrollSection
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: If4261a3e32375f6127241b846b97a3b4ac29eb0b

--- browser/src/layer/tile/ScrollSection.ts
+++ browser/src/layer/tile/ScrollSection.ts
@@ -13,7 +13,9 @@ export class ScrollSection extends CanvasSectionObject {
 	pendingScrollEvent: any = null;
 	stepByStepScrolling: boolean = false; // quick scroll will move "page up/down" not "jump to"
 
-	constructor () {
+	isRTL: () => boolean;
+
+	constructor (isRTL?: () => boolean) {
 		super({
 			name: L.CSections.Scroll.name,
 			anchor: [],
@@ -31,6 +33,8 @@ export class ScrollSection extends CanvasSectionObject {
 		this.sectionProperties = {};
 
 		this.map = L.Map.THIS;
+
+		this.isRTL = isRTL ?? (() => false);
 
 		this.map.on('scrollto', this.onScrollTo, this);
 		this.map.on('scrollby', this.onScrollBy, this);
@@ -693,6 +697,10 @@ export class ScrollSection extends CanvasSectionObject {
 	}
 
 	public scrollHorizontalWithOffset (offset: number): void {
+		if (this.isRTL()) {
+			offset = -offset;
+		}
+
 		var go = true;
 		if (offset > 0) {
 			if (this.documentTopLeft[0] + offset > this.sectionProperties.xMax)
@@ -1054,6 +1062,6 @@ export class ScrollSection extends CanvasSectionObject {
 
 }
 
-L.getNewScrollSection = function () {
-	return new lool.ScrollSection();
+L.getNewScrollSection = function (isRTL?: () => boolean) {
+	return new lool.ScrollSection(isRTL);
 };
