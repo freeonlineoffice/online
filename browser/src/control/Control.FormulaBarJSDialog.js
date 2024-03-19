776bc33550bdff0922e3735f233d4bf9039c598a
Szymon Kłos <szymon.klos@collabora.com>
2024-03-19 13:30:24 +0100

    formulabar: make it a grid to not move on activation
    this puts buttons and inputs at fixed positions so they
    don't move when we enter formula editing mode
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I2dd99e3ac233b66847af0165359d15fb50a10a97

--- browser/src/control/Control.FormulaBarJSDialog.js
+++ browser/src/control/Control.FormulaBarJSDialog.js
@@ -101,7 +101,7 @@ class FormulaBar {
 					text: _('cell address')
 				},
 				{
-					id: 'formulabar-toolbox',
+					id: 'formulabar-buttons-toolbox',
 					type: 'toolbox',
 					children: [
 						{
@@ -133,7 +133,13 @@ class FormulaBar {
 							type: 'customtoolitem',
 							text: _UNO('.uno:Cancel', 'spreadsheet'),
 							visible: false
-						},
+						}
+					]
+				},
+				{
+					id: 'formulabar-toolbox',
+					type: 'toolbox',
+					children: [
 						{
 							id: 'sc_input_window',
 							type: 'formulabaredit',
