9defd1a2842199b219c6ab32cb8fc6a7e0335dcf
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2024-03-27 14:54:52 +0530

    User avatar should be consistent after mode change
    - User avatar should first consider is there is already extract info available or not
    - if we already have that image source data then please do not set default value
    - also when mode change we do preform some refresh function on all icons
        - `map.on('themechanged', setupIcon, this);`
    - in that case as well, first check if we have user extract info or not
    - and based on that set image source URL
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    
    Change-Id: Ifbd543322f5222fa7717b40a5ae565ed2f3c9891
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>

--- browser/src/layer/tile/CommentListSection.ts
+++ browser/src/layer/tile/CommentListSection.ts
@@ -454,7 +454,7 @@ export class CommentSection extends CanvasSectionObject {
 		var tdImg = L.DomUtil.create(tagTd, 'lool-annotation-img', tr);
 		var tdAuthor = L.DomUtil.create(tagTd, 'lool-annotation-author', tr);
 		var imgAuthor = L.DomUtil.create('img', 'avatar-img', tdImg);
-		L.LOUtil.setImage(imgAuthor, 'user.svg', this.map);
+		L.LOUtil.setUserImage(imgAuthor, this.map);
 		imgAuthor.setAttribute('width', 32);
 		imgAuthor.setAttribute('height', 32);
 		var authorAvatarImg = imgAuthor;
