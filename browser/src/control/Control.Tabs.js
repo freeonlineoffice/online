d8e3e3d8a645b61f9fc3aacb63c7449cf040fd3e
Henry Castro <hcastro@collabora.com>
2024-03-27 08:12:19 -0400

    browser: fix scroll tab when loading document
    If the element 'spreadsheet-tab-scroll' does not exist
    when loads document, then scroll to the selected tab.
    
    Change-Id: Iec86225f7ea97a149d455210921daaf91a4e761a
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/control/Control.Tabs.js
+++ browser/src/control/Control.Tabs.js
@@ -208,7 +208,7 @@ L.Control.Tabs = L.Control.extend({
 							}(i).bind(this));
 					}
 
-					if (i === selectedPart) {
+					if (!scrollDiv && i === selectedPart) {
 						horizScrollPos = tab.offsetLeft;
 					}
 
