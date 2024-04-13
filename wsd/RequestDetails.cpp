60ca0ae591185ee48de12b6a179fef4cb9ed6d53
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-04-13 16:18:48 +0100

    cid#360692 Uninitialized scalar variable
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ica6d5a202c1124680ae5ab01bd551fc800a74910

--- wsd/RequestDetails.cpp
+++ wsd/RequestDetails.cpp
@@ -65,7 +65,7 @@ RequestDetails::RequestDetails(Poco::Net::HTTPRequest &request, const std::strin
     _isGet = method == "GET";
     _isHead = method == "HEAD";
     auto it = request.find("ProxyPrefix");
-	_isProxy = it != request.end();
+    _isProxy = it != request.end();
     if (_isProxy)
         _proxyPrefix = it->second;
     it = request.find("Upgrade");
@@ -90,6 +90,10 @@ RequestDetails::RequestDetails(const std::string &mobileURI)
 
 RequestDetails::RequestDetails(const std::string& wopiSrc, const std::vector<std::string>& options,
                                const std::string& compat)
+    : _isGet(true)
+    , _isHead(false)
+    , _isProxy(false)
+    , _isWebSocket(false)
 {
     // /lool/<encoded-document-URI+options>/ws?WOPISrc=<encoded-document-URI>&compat=/ws[/<sessionId>/<command>/<serial>]
 
