8e03d5027f5b4b74a1170d5eb8581272937368bc
Michael Meeks <michael.meeks@collabora.com>
2024-04-17 21:10:27 +0100

    bgsave: improve input processing trace debugging.
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I53ff0809248f010b5a569ef83d8d02f3287a77b5

--- common/MessageQueue.hpp
+++ common/MessageQueue.hpp
@@ -75,6 +75,11 @@ public:
         return _queue.empty();
     }
 
+    bool size()
+    {
+        return _queue.size();
+    }
+
     /// Thread safe removal of all the pending messages.
     void clear()
     {
