514c261eebb5b5e3b10f17bac47a0c68711d9129
Pranam Lashkari <lpranam@collabora.com>
2024-04-24 02:29:38 +0100

    annotation: avoid explicit unselect before select
    select function now can take care of reselection and layout update
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I2a061643ec190d5b2d005b940923a28131ed6cb0

--- browser/src/layer/tile/CommentListSection.ts
+++ browser/src/layer/tile/CommentListSection.ts
@@ -623,9 +623,8 @@ export class CommentSection extends CanvasSectionObject {
 			}
 		}
 		else {
-			this.unselect();
 			annotation.reply();
-			this.select(annotation);
+			this.select(annotation, true);
 			annotation.focus();
 		}
 	}
@@ -641,11 +640,6 @@ export class CommentSection extends CanvasSectionObject {
 			}.bind(this), /* isMod */ true);
 		}
 		else {
-			if (this.sectionProperties.docLayer._docType !== 'spreadsheet') {
-				this.unselect();
-				this.select(annotation);
-			}
-
 			// Make sure that comment is not transitioning and comment menu is not open.
 			var tempFunction = function() {
 				setTimeout(function() {
