23138eaa66ad4924361ce338e6b6f49a3dedcf38
Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
2024-05-01 22:41:41 +0530

    initializ UI after server reconnection
    - Non-edit mode does not intialized after server reconnection
    - this patch will fix initializedUI after reconnection
    Signed-off-by: Darshan-upadhyay1110 <darshan.upadhyay@collabora.com>
    Change-Id: I43405067287dd3f7f134fc9dcabfdcae183724ce

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -1403,6 +1403,8 @@ app.definitions.Socket = L.Class.extend({
 			this._map.setPermission(app.file.permission);
 			window.migrating = false;
 			this._map.uiManager.initializeSidebar();
+			if (typeof window.initializedUI === 'function')
+				window.initializedUI();
 		}
 
 		this._map.fire('docloaded', {status: true});
