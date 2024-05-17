ebbc49acb4f849b57e383adc4dc2cbd026170ac2
Michael Meeks <michael.meeks@collabora.com>
2024-05-17 10:27:53 +0100

    callback logging fix mis-placed brace.
    Causing excessive logging in error; we would find the right session
    next time around the loop; from:
    
        commit 6f49f9398e237c46c71910915bf11925b85fd7a7
        Author: Michael Meeks <michael.meeks@collabora.com>
        Date:   Thu May 9 09:19:44 2024 +0100
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I33f696e4fae899a896d94cbeb70d5d9306f3b414

--- kit/Kit.cpp
+++ kit/Kit.cpp
@@ -2176,13 +2176,12 @@ void Document::drainCallbacks()
                 if (!broadcast)
                     break;
             }
-
-            if (!isFound)
-                LOG_ERR("Document::ViewCallback. Session [" << viewId <<
-                        "] is no longer active to process [" << lokCallbackTypeToString(type) <<
-                        "] [" << LOOLProtocol::getAbbreviatedMessage(payload) <<
-                        "] message to Master Session.");
         }
+        if (!isFound)
+            LOG_ERR("Document::ViewCallback. Session [" << viewId <<
+                    "] is no longer active to process [" << lokCallbackTypeToString(type) <<
+                    "] [" << LOOLProtocol::getAbbreviatedMessage(payload) <<
+                    "] message to Master Session.");
     }
 
     if (_websocketHandler)
