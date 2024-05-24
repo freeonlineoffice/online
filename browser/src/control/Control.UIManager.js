55d74943489c884713cb8ec3773f5a281cc6b971
Gülşah Köse <gulsah.kose@collabora.com>
2024-05-24 14:39:11 +0300

    Implement Invert background color button
    Button invokes the uno:InvertBackground command than user can
    see the inverted document color even app is dark mode.
    
    Signed-off-by: Gülşah Köse <gulsah.kose@collabora.com>
    Change-Id: Ie273ec4b3d26ffbc38384dc39943b8656e4364d4

--- browser/src/control/Control.UIManager.js
+++ browser/src/control/Control.UIManager.js
@@ -100,7 +100,7 @@ L.Control.UIManager = L.Control.extend({
 	},
 
 	invertBackground: function() {
-		// Implement this
+		app.socket.sendMessage('uno .uno:InvertBackground');
 	},
 
 	toggleDarkMode: function() {
