0bc2c26880101f701fa179e835ace6c756aa7ee5
Pranam Lashkari <lpranam@collabora.com>
2024-07-07 20:47:58 +0100

    annotation: remove initial comment animation
    when entire comment list was imported initially
    or after showing hiding changes, animation
    was played multiple times which was annoying
    and distracting to users
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I8a3da3cd551288bef94f5627ca11effd85bf99d0

--- browser/src/canvas/sections/CommentListSection.ts
+++ browser/src/canvas/sections/CommentListSection.ts
@@ -60,6 +60,7 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 		commentsAreListed: boolean;
 		[key: string]: any;
 	};
+	disableLayoutAnimation: boolean = false;
 
 	map: any;
 	static autoSavedComment: lool.Comment;
@@ -1588,12 +1589,16 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 		return (this.sectionProperties.docLayer._docType === 'presentation' || this.sectionProperties.docLayer._docType === 'drawing') && !app.file.fileBasedView;
 	}
 
+	private getAnimationDuration() :number {
+		return this.disableLayoutAnimation ? 0 : undefined; // undefined means it will use default value
+	}
+
 	private layoutUp (subList: any, actualPosition: Array<number>, lastY: number): number {
 		var height: number;
 		for (var i = 0; i < subList.length; i++) {
 			height = subList[i].sectionProperties.container.getBoundingClientRect().height;
 			lastY = subList[i].sectionProperties.data.anchorPix[1] + height < lastY ? subList[i].sectionProperties.data.anchorPix[1]: lastY - (height * app.dpiScale);
-			(new L.PosAnimation()).run(subList[i].sectionProperties.container, {x: Math.round(actualPosition[0] / app.dpiScale), y: Math.round(lastY / app.dpiScale)});
+			(new L.PosAnimation()).run(subList[i].sectionProperties.container, {x: Math.round(actualPosition[0] / app.dpiScale), y: Math.round(lastY / app.dpiScale)}, this.getAnimationDuration());
 			if (!subList[i].isEdit())
 				subList[i].show();
 		}
@@ -1639,9 +1644,9 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 			var isRTL = document.documentElement.dir === 'rtl';
 
 			if (selectedComment)
-				(new L.PosAnimation()).run(subList[i].sectionProperties.container, {x: Math.round(actualPosition[0] / app.dpiScale) - this.sectionProperties.deflectionOfSelectedComment * (isRTL ? -1 : 1), y: Math.round(lastY / app.dpiScale)});
+				(new L.PosAnimation()).run(subList[i].sectionProperties.container, {x: Math.round(actualPosition[0] / app.dpiScale) - this.sectionProperties.deflectionOfSelectedComment * (isRTL ? -1 : 1), y: Math.round(lastY / app.dpiScale)}, this.getAnimationDuration());
 			else
-				(new L.PosAnimation()).run(subList[i].sectionProperties.container, {x: Math.round(actualPosition[0] / app.dpiScale), y: Math.round(lastY / app.dpiScale)});
+				(new L.PosAnimation()).run(subList[i].sectionProperties.container, {x: Math.round(actualPosition[0] / app.dpiScale), y: Math.round(lastY / app.dpiScale)}, this.getAnimationDuration());
 
 			lastY += (subList[i].sectionProperties.container.getBoundingClientRect().height * app.dpiScale);
 			if (!subList[i].isEdit())
@@ -1792,6 +1797,8 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 		}
 		else
 			app.view.size.pixels[1] = app.file.size.pixels[1];
+
+		this.disableLayoutAnimation = false;
 	}
 
 	private layout (zoom: any = null): void {
@@ -1978,7 +1985,7 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 								// move up
 								var posX = this.sectionProperties.commentList[i].sectionProperties.container._leaflet_pos.x;
 								var posY = this.sectionProperties.commentList[i].sectionProperties.container._leaflet_pos.y-moveUp;
-								(new L.PosAnimation()).run(this.sectionProperties.commentList[i].sectionProperties.container, {x: Math.round(posX), y: Math.round(posY)});
+								(new L.PosAnimation()).run(this.sectionProperties.commentList[i].sectionProperties.container, {x: Math.round(posX), y: Math.round(posY)}, this.getAnimationDuration());
 								// increase comment height
 								maxSize += moveUp;
 							}
@@ -2075,6 +2082,7 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 	}
 
 	public importComments (commentList: any): void {
+		this.disableLayoutAnimation = true;
 		var comment;
 		if (Comment.isAnyEdit()) {
 			this.map.uiManager.showConfirmModal(
