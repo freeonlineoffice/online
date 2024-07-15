fd6f37dfbce2128a7d769c39115816383d5a8e0d
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-07-15 14:29:22 +0100

    fix UnitWOPISaveOnExit test with mount namespaces enabled
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I8f6ac58b064ecff8a74a694736fd85c627be0a27

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -1162,7 +1162,9 @@ bool DocumentBroker::doDownloadDocument(const Authorization& auth,
     }
 #endif //!MOBILEAPP
 
-    const std::string localFilePath = Poco::Path(getJailRoot(), localPath).toString();
+    const std::string localFilePath = Poco::Path(FileUtil::buildLocalPathToJail(COOLWSD::EnableMountNamespaces,
+                                                                                getJailRoot(),
+                                                                                localPath)).toString();
     std::ifstream istr(localFilePath, std::ios::binary);
     Poco::SHA1Engine sha1;
     Poco::DigestOutputStream dos(sha1);
