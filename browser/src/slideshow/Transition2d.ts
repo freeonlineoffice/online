b5a1f657d5f8e40611f83fc36de5e59aff8068ac
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-07-17 16:25:37 +0530

    Slideshow: Reduce Duplicated Code
    - Remove redundant vertex shader.
    - Call prepareTransition from Transition2d.
    - Fix canvas width for VenetianTransition.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: Iaa06b0f63726a6e8a982f7e3df68d0e4b4c47e82

--- browser/src/slideshow/Transition2d.ts
+++ browser/src/slideshow/Transition2d.ts
@@ -55,6 +55,8 @@ class Transition2d {
 
 		this.time = 0;
 		this.startTime = null;
+
+		this.prepareTransition();
 	}
 
 	public getVertexShader(): string {
@@ -97,6 +99,10 @@ class Transition2d {
 	public startTransition(): void {
 		this.startTime = performance.now();
 		requestAnimationFrame(this.render.bind(this));
+	}
+
+	public start(): void {
+		this.startTransition();
 	}
 
 	public initBuffers(): void {
