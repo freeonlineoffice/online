fa4e68b1c25bce52d2e6ebb9b4b809e7b0496cfc
Marco Cecchetti <marco.cecchetti@collabora.com>
2024-07-31 11:31:06 +0200

    slideshow rework: not possible to navigate when presenting in a window
    Slide navigation with keys doesn't work when presenting in a window.
    
    Signed-off-by: Marco Cecchetti <marco.cecchetti@collabora.com>
    Change-Id: I149c9f6eb9e25fe792e2bbfb7376e1a6f30c4d88

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -113,16 +113,18 @@ class SlideShowPresenter {
 
 	_onFullScreenChange() {
 		this._fullscreen = document.fullscreenElement;
-		if (!this._fullscreen) {
-			this._stopFullScreen();
-		} else {
+		if (this._fullscreen) {
+			window.addEventListener('keydown', this._onCanvasKeyDown.bind(this));
 			this.centerCanvas();
+		} else {
+			this._stopFullScreen();
 		}
 	}
 
 	_stopFullScreen() {
 		if (!this._slideShowCanvas) return;
 
+		window.removeEventListener('keydown', this._onCanvasKeyDown.bind(this));
 		L.DomUtil.remove(this._slideShowCanvas);
 		this._slideShowCanvas = null;
 		if (this._presenterContainer) {
@@ -168,8 +170,10 @@ class SlideShowPresenter {
 	}
 
 	_onCanvasKeyDown(event: KeyboardEvent) {
-		if (event.code === 'Space') this._nextSlide();
-		else if (event.code === 'Backspace') this._previoustSlide();
+		if (event.code === 'Space' || event.code === 'ArrowRight')
+			this._nextSlide();
+		else if (event.code === 'Backspace' || event.code === 'ArrowLeft')
+			this._previoustSlide();
 	}
 
 	private centerCanvas() {
@@ -226,7 +230,6 @@ class SlideShowPresenter {
 		canvas.id = 'slideshow-canvas';
 
 		canvas.addEventListener('click', this._onCanvasClick.bind(this));
-		window.addEventListener('keydown', this._onCanvasKeyDown.bind(this));
 
 		try {
 			this._slideRenderer = new SlideRendererGl(canvas);
@@ -383,6 +386,10 @@ class SlideShowPresenter {
 		this._slideShowWindowProxy.addEventListener(
 			'resize',
 			this.onSlideWindowResize.bind(this),
+		);
+		this._slideShowWindowProxy.addEventListener(
+			'keydown',
+			this._onCanvasKeyDown.bind(this),
 		);
 
 		const slideShowWindow = this._slideShowWindowProxy;
