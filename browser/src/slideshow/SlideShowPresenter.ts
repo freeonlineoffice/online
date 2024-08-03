27ac63aa63d1184fa352730261a746168ed6e492
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-08-03 16:31:08 +0530

    Slideshow: Improve performance by replacing initial black screen with pixel rendering
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: I86afe9658e8e3aad36067f89296dfd7ede2ff5fa

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -355,10 +355,7 @@ class SlideShowPresenter {
 			slideInfo.transitionType != 'NONE'
 		) {
 			// generate empty black slide
-			const blankTexture = this._slideRenderer.createEmptyTexture(
-				this._presentationInfo.docWidth,
-				this._presentationInfo.docHeight,
-			);
+			const blankTexture = this._slideRenderer.createEmptyTexture();
 
 			this._doTransition(blankTexture, this._currentSlide);
 		} else {
