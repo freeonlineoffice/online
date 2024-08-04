a0a4737db14a408a63d09760c05676ef6daa59a8
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-08-04 18:25:36 -0400

    killpoco: remove unused http helpers
    Adapt patch to LibreOffice Online.
    
    Change-Id: Ifb6fffdef5d00cf8c3015972c48e1c14ad6bc0dc
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/Storage.hpp
+++ wsd/Storage.hpp
@@ -28,15 +28,6 @@
 /// Limits number of HTTP redirections to prevent from redirection loops
 static constexpr auto RedirectionLimit = 21;
 
-namespace Poco
-{
-namespace Net
-{
-class HTTPClientSession;
-}
-
-} // namespace Poco
-
 struct LockContext;
 
 /// Base class of all Storage abstractions.
@@ -412,7 +403,6 @@ public:
     static std::unique_ptr<StorageBase> create(const Poco::URI& uri, const std::string& jailRoot,
                                                const std::string& jailPath, bool takeOwnership);
 
-    static Poco::Net::HTTPClientSession* getHTTPClientSession(const Poco::URI& uri);
     static std::shared_ptr<http::Session> getHttpSession(const Poco::URI& uri);
 
 protected:
