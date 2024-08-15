136b853cefeb5df5ef472ff263c5770619827eae
Sven Göthel <sven.gothel@collabora.com>
2024-08-15 12:45:19 +0200

    cool#9621: Keep Connection Open for files served via FileServerRequestHandler and ClientRequestDispatcher
    - Removed socket->shutdown()
      - ClientRequestDispatcher::handleIncomingMessage()
        - post FileServerRequestHandler::handleRequest()
      - ClientRequestDispatcher::handleRootRequest()
      - ClientRequestDispatcher::handleWopiDiscoveryRequest()
      - ClientRequestDispatcher::handleRobotsTxtRequest()
      - sendCapabilities()
    
    - Removed Header `Connection: close`
      - FileServerRequestHandler::handleRequest() general case serving files (tail)
      - FileServerRequestHandler::preprocessFile(..)
      - ClientRequestDispatcher::handleRootRequest()
      - ClientRequestDispatcher::handleRobotsTxtRequest()
    
    - Using http::Header::ConnectionToken::Close instead of string (get/set)
    
    - UnitSession tests
      - ClientRequestDispatcher
        - /
        - /favicon.ico
        - /hosting/discovery
        - /hosting/capabilities
        - /robots.txt
        -
      - FileServerRequestHandler
        - /browser/dist/cool.html
        - /browser/dist/cool.css
    
    Signed-off-by: Sven Göthel <sven.gothel@collabora.com>
    Change-Id: I83c09e0a8101931efece42e42e5a8e3abd7e696f

--- net/HttpRequest.hpp
+++ net/HttpRequest.hpp
@@ -1468,7 +1468,7 @@ private:
                 }
             }
 
-            if (_response->get("Connection", "") == "close")
+            if( _response->header().getConnectionToken() == Header::ConnectionToken::Close )
             {
                 LOG_TRC("Our peer has sent the 'Connection: close' token. Disconnecting.");
                 onDisconnect();
--- wsd/FileServer.cpp
+++ wsd/FileServer.cpp
@@ -569,8 +569,9 @@ void FileServerRequestHandler::handleRequest(const HTTPRequest& request,
 
         // Is this a file we read at startup - if not; it's not for serving.
         if (FileHash.find(relPath) == FileHash.end() &&
-            FileHash.find(relPath + ".br") == FileHash.end())
-            throw Poco::FileNotFoundException("Invalid URI request: [" + requestUri.toString() + "].");
+            FileHash.find(relPath + ".br") == FileHash.end()) {
+            throw Poco::FileNotFoundException("Invalid URI request (hash): [" + requestUri.toString() + "].");
+        }
 
         if (endPoint == "welcome.html")
         {
@@ -711,7 +712,6 @@ void FileServerRequestHandler::handleRequest(const HTTPRequest& request,
                 content = getUncompressedFile(relPath);
 
             response.add("Content-Length", std::to_string(content->size()));
-            response.add("Connection", "close");
 
             if (!noCache)
             {
@@ -726,7 +726,6 @@ void FileServerRequestHandler::handleRequest(const HTTPRequest& request,
 
             socket->send(response);
             socket->send(*content);
-            // shutdown by caller
         }
     }
     catch (const Poco::Net::NotAuthenticatedException& exc)
@@ -1389,6 +1388,7 @@ FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::prepro
         {
             if (!HttpHelper::verifyWOPISrc(request.getURI(), param.second, socket))
             {
+                httpResponse.header().setConnectionToken(http::Header::ConnectionToken::Close);
                 return ResourceAccessDetails();
             }
 
@@ -1516,7 +1516,6 @@ FileServerRequestHandler::ResourceAccessDetails FileServerRequestHandler::prepro
         }
     }
 
-    httpResponse.add("Connection", "close");
     httpResponse.setBody(preprocess, mimeType);
 
     socket->send(httpResponse);
