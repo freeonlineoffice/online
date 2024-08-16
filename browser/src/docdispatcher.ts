01fa1e77d589675ea7454b196d0b8c980649a00d
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-08-16 17:22:26 +0530

    slideshow: enable new canvas on debug mode for presentation and present-in-window
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: I6bda137a72330ee91d79c3473797c9f22e150b2f

--- browser/src/docdispatcher.ts
+++ browser/src/docdispatcher.ts
@@ -387,6 +387,19 @@ class Dispatcher {
 			else app.map.fire('fullscreen');
 		};
 
+		this.actionsMap['presentation-currentslide'] = this.actionsMap[
+			'presentation-currentslide'
+		] = () => {
+			if (app.map._debug.debugOn)
+				app.map.fire('newfullscreen', {
+					startSlideNumber: app.map.getCurrentPartNumber(),
+				});
+			else
+				app.map.fire('fullscreen', {
+					startSlideNumber: app.map.getCurrentPartNumber(),
+				});
+		};
+
 		this.actionsMap['presentinwindow'] = this.actionsMap[
 			'present-in-window'
 		] = () => {
