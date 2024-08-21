82b6373d93c0d46a17412b4258f4e53245a34d0d
Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
2024-08-21 10:24:02 +0530

    slideshow: enable close present window at the end of slideshow
    Signed-off-by: Javiya Vivekkumar Dineshbhai <vivek.javiya@collabora.com>
    Change-Id: Ic9ac36f91de223b04ab6af8939a8139c7d65fdc8

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -169,6 +169,7 @@ class SlideShowPresenter {
 			const currSlideInfo = this.getSlideInfo(this._currentSlide);
 			if (!currSlideInfo.isEndless) {
 				this._stopFullScreen();
+				this._closeSlideShowWindow();
 				return;
 			}
 
@@ -449,6 +450,12 @@ class SlideShowPresenter {
 			</body>
 			</html>
 			`;
+	}
+
+	_closeSlideShowWindow() {
+		this._slideShowWindowProxy.opener.focus();
+		this._slideShowWindowProxy.close();
+		this._map.uiManager.closeSnackbar();
 	}
 
 	_doPresentation(isStarting = false) {
