8cb337d6abb0560bb78fb404e31760f650a3f324
Szymon Kłos <szymon.klos@collabora.com>
2024-08-21 19:44:49 +0200

    notebookbar: remove menubartoolitem for zoomin
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ibf40b80b77a50f8913a26f35e9cc111dac6819b7

--- browser/src/control/Control.NotebookbarCalc.js
+++ browser/src/control/Control.NotebookbarCalc.js
@@ -1176,9 +1176,8 @@ L.Control.NotebookbarCalc = L.Control.NotebookbarWriter.extend({
 							{
 								'id': 'zoomin',
 								'class': 'unozoomin',
-								'type': 'menubartoolitem',
+								'type': 'customtoolitem',
 								'text': _UNO('.uno:ZoomPlus'),
-								'command': '.uno:ZoomPlus',
 								'accessibility': { focusBack: true,	combination: 'ZI', de: null }
 							}
 						]
