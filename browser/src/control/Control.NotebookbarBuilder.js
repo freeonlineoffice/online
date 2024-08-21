ed21a8a69b527d6978222505417b0d924b7d085d
Szymon Kłos <szymon.klos@collabora.com>
2024-08-21 20:06:11 +0200

    notebookbar: remove menubartoolitem for downloads in Draw
    Unify Draw with other applications so it will use menu
    not separate buttons for download entries
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I06f0b6f07af91b975645d5278d2380fa70b35f72

--- browser/src/control/Control.NotebookbarBuilder.js
+++ browser/src/control/Control.NotebookbarBuilder.js
@@ -467,6 +467,17 @@ L.Control.NotebookbarBuilder = L.Control.JSDialogBuilder.extend({
 					'command': 'exportpdf'
 				}
 			] : []);
+		} else if (docType === 'drawing') {
+			submenuOpts = [
+				{
+					'action': 'downloadas-odg',
+					'text': _('ODF Drawing (.odg)')
+				},
+				{
+					'action': 'downloadas-png',
+					'text': _('Image (.png)')
+				}
+			];
 		}
 
 		submenuOpts.forEach(function mapIconToItem(menuItem) {
