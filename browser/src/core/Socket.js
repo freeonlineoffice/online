5ffe227b388c4edb5b1a243390cf4fb80872ae6d
Szymon Kłos <szymon.klos@collabora.com>
2024-08-29 09:31:09 +0200

    cypress: fail on exceptions even without debug
    this will fail on CI jobs - protecting us from
    breaking local testing
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Id36f16909789a97c8e90e3eb448b70e16f2fe95e

--- browser/src/core/Socket.js
+++ browser/src/core/Socket.js
@@ -405,7 +405,7 @@ app.definitions.Socket = L.Class.extend({
 						window.app.console.error(msg);
 
 						// When debugging let QA know something is up.
-						if (window.enableDebug)
+						if (window.enableDebug || L.Browser.cypressTest)
 							this._map.uiManager.showInfoModal(
 								'lool_alert', '', msg, '', _('Close'), function() { /* Do nothing. */ }, false);
 
