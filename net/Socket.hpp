8b2bbc5d2330e739dd1d0b2d9d10a9ddc5c814de
Sven Göthel <sven.gothel@collabora.com>
2024-09-09 07:13:26 +0200

    C++ Misc Cleanup: More override, std-header, std::make_* usage, ..
    - Use override for virtual function overrides (incl. dtor)
    - Use C++ std headers, e.g. cctype instead of ctype.h
    - Use global anonym namespace for system functions
      or std:: system functions, whichever is applicable
    - Use std::make_shared<StreamSocket>(..)
    - Use std::make_unique<Watchdog>()
    - Use 'const' where applicable
    - Use pre-incr where applicable
    - Use typename where applicable
    - Drop redundant type conversions
    
    Signed-off-by: Sven Göthel <sven.gothel@collabora.com>
    Change-Id: Id844187c82fce3c6e9bceb512c07180705608ebc

--- net/Socket.hpp
+++ net/Socket.hpp
@@ -966,7 +966,7 @@ public:
         LOG_TRC("StreamSocket ctor");
     }
 
-    ~StreamSocket()
+    ~StreamSocket() override
     {
         LOG_TRC("StreamSocket dtor called with pending write: " << _outBuffer.size()
                                                                 << ", read: " << _inBuffer.size());
