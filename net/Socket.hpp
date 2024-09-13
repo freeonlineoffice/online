531016a5a4aea46760b8dc6286688fcca320528d
Sven Göthel <sven.gothel@collabora.com>
2024-09-13 21:06:38 +0100

    handshakeFail doesn't need to be public
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I6fc7b1b54ee035f17b52146b51a0f792fa5a10cb

--- net/Socket.hpp
+++ net/Socket.hpp
@@ -1446,12 +1446,6 @@ protected:
             disposition.setClosed();
     }
 
-    void handshakeFail()
-    {
-        if (_socketHandler)
-            _socketHandler->onHandshakeFail();
-    }
-
 public:
     /// Override to write data out to socket.
     /// Returns the last return from writeData.
@@ -1517,6 +1511,12 @@ public:
     void dumpState(std::ostream& os) override;
 
 protected:
+    void handshakeFail()
+    {
+        if (_socketHandler)
+            _socketHandler->onHandshakeFail();
+    }
+
     /// Reads data with file descriptors as control data if received.
     /// Can be used only with Unix sockets.
     int readFDs(char* buf, int len, std::vector<int>& fds)
