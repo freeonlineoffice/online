8e4d1ca8227cc0cd8cae2586853ebaca2a8ec62d
Rashesh <rashesh.padia@collabora.com>
2024-10-14 16:00:09 +0530

    refactor: move 'openMentionPopup' & 'closeMentionPopup' to Mention.ts
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: Idc5e96f61dfcf78a109612813e296978701fe176

--- browser/src/control/Control.FormulaUsagePopup.ts
+++ browser/src/control/Control.FormulaUsagePopup.ts
@@ -51,7 +51,7 @@ class FormulaUsagePopup extends L.Control.AutoCompletePopup {
 	}
 
 	openFormulaUsagePopup(ev: FireEvent) {
-		this.openMentionPopup({ data: ev });
+		this.openPopup({ data: ev });
 		this.map.focus();
 	}
 
