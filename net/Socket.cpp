574ab11e4da25641c826977221075da39a413dbf
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2024-10-21 05:26:23 -0400

    wsd: missing new-line in SocketPoll::dumpState
    Change-Id: I6ee200cd01b351759f83440a6200b29b5b09e69f
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- net/Socket.cpp
+++ net/Socket.cpp
@@ -1173,7 +1173,7 @@ void SocketPoll::dumpState(std::ostream& os) const
     os << "\t    fd\tevents\trbuffered\twbuffered\trtotal\twtotal\tclientaddress\n";
     for (const std::shared_ptr<Socket>& socket : pollSockets)
         socket->dumpState(os);
-    os << "\n  Done [" << name() << ']';
+    os << "\n  Done [" << name() << "]\n";
 }
 
 /// Returns true on success only.
