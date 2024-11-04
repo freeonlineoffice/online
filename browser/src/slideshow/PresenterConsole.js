ce2398c22381ef67513fca211432a22f5ef40b20
Henry Castro <hcastro@collabora.com>
2024-11-04 09:16:52 -0400

    browser: slideshow: handle click event on slides
    Adapt patch to LibreOffice Online.
    
    Change-Id: I2c247a14e4ce2eed3be6c2d4794353fb490354be
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -317,6 +317,10 @@ class PresenterConsole {
 		elem.style.height = '80vh';
 		elem.style.width = '100%';
 		this._slides.appendChild(elem);
+		this._slides.addEventListener(
+			'click',
+			L.bind(this._onClickSlides, this),
+		);
 		elem = this._proxyPresenter.document.createElement('div');
 		elem.style.textAlign = 'center';
 		button = this._proxyPresenter.document.createElement('button');
@@ -477,12 +481,33 @@ class PresenterConsole {
 	_onHideSlides(e) {
 		let elem =
 			this._proxyPresenter.document.querySelector('#main-content');
+		let selection = this._proxyPresenter.document.getSelection();
+		if (selection && selection.rangeCount > 1) {
+			let range = selection.getRangeAt(0);
+			if (range) {
+				this._presenter
+					.getNavigator()
+					.displaySlide(range.startOffset, true);
+			}
+		}
+
 		this._slides.remove();
 		elem.appendChild(this._first);
 		elem.appendChild(this._second);
 
 		elem = this._proxyPresenter.document.querySelector('#slides');
 		elem.disable = false;
+		e.stopPropagation();
+	}
+
+	_onClickSlides(e) {
+		if (e.target && e.target.localName === 'img') {
+			this._proxyPresenter.document.getSelection().empty();
+			let range = document.createRange();
+			range.selectNode(e.target);
+			this._proxyPresenter.document.getSelection().addRange(range);
+		}
+
 		e.stopPropagation();
 	}
 
