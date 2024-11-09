29f71da9585c4e960379ee4c2ce2c9630bfcf18f
Sven Göthel <sven.gothel@collabora.com>
2024-11-09 05:08:25 +0100

    Add max/used concurrent TCP connections to Prometheus text-based 'getMetrics' REST endpoint
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Sven Göthel <sven.gothel@collabora.com>
    Change-Id: I68dccec09e7907a5d3561a3df10637b30815bcc1

--- wsd/AdminModel.cpp
+++ wsd/AdminModel.cpp
@@ -1138,6 +1138,7 @@ void AdminModel::getMetrics(std::ostringstream &oss)
     oss << "loolwsd_thread_count " << Util::getStatFromPid(getpid(), 19) << std::endl;
     oss << "loolwsd_cpu_time_seconds " << Util::getCpuUsage(getpid()) / sysconf (_SC_CLK_TCK) << std::endl;
     oss << "loolwsd_memory_used_bytes " << Util::getMemoryUsagePSS(getpid()) * 1024 << std::endl;
+    oss << "loolwsd_tcp_connections_used " << StreamSocket::getExternalConnectionCount() << std::endl;
     oss << std::endl;
 
     oss << "forkit_count " << getPidsFromProcName(std::regex("forkit"), nullptr) << std::endl;
