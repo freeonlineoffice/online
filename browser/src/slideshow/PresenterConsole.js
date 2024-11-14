2beb133400996d81b4fcba77c295b3e85c368c91
Henry Castro <hcastro@collabora.com>
2024-11-14 09:21:29 -0400

    browser: slideshow: enable/disable presenter console
    Change-Id: I694dd30f1762b2b7885cbfbd121a8e5ddf42e8da
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -179,6 +179,10 @@ class PresenterConsole {
 			return;
 		}
 
+		this._map.fire('enablecommand', {
+			controlId: 'view-presentation-in-console',
+			enable: false,
+		});
 		this._map.off('newpresentinconsole', this._onPresentInConsole, this);
 
 		this._proxyPresenter.document.open();
@@ -745,6 +749,10 @@ class PresenterConsole {
 		this._map.off('transitionend', this._onTransitionEnd, this);
 		this._map.off('tilepreview', this._onTilePreview, this);
 		this._map.on('newpresentinconsole', this._onPresentInConsole, this);
+		this._map.fire('enablecommand', {
+			controlId: 'view-presentation-in-console',
+			enable: true,
+		});
 	}
 
 	_onResize() {
