a955c7f926417d62c4d91d5ba93a2d1c0c8d6062
Henry Castro <hcastro@collabora.com>
2024-11-18 17:28:24 -0400

    browser: slideshow: fix click selection image
    Update to latest UI change adding the arrow back button
    .
    Change-Id: Ic6684f86c80c88ba834804ed3421ce5335632b2d
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -716,6 +716,10 @@ class PresenterConsole {
 		);
 		elem.appendChild(this._slides);
 
+		if (this._selectedImg) {
+			this._selectedImg.scrollIntoView();
+		}
+
 		let preview;
 		for (let index = 0; index < this._previews.length; index++) {
 			preview = this._previews[index];
@@ -732,7 +736,7 @@ class PresenterConsole {
 		}
 	}
 
-	_onHideSlides(img) {
+	_onHideSlides() {
 		let elem = this._proxyPresenter.document.querySelector(
 			'#presentation-content',
 		);
@@ -753,18 +757,32 @@ class PresenterConsole {
 			this._proxyPresenter.document.querySelector('#close-slides');
 		closeSlideButton.style.display = 'none';
 
-		if (img && this._proxyPresenter) {
+		if (this._selectedImg && this._proxyPresenter) {
 			this._proxyPresenter.requestAnimationFrame(
 				function (navigator, index) {
 					navigator.displaySlide(index, true);
-				}.bind(null, this._presenter.getNavigator(), img._index),
+				}.bind(null, this._presenter.getNavigator(), this._selectedImg._index),
 			);
+		}
+	}
+
+	_selectImg(img) {
+		if (this._selectedImg) {
+			this._selectedImg.style.border = '';
+			this._selectedImg.style.borderColor = '';
+			this._selectedImg = null;
+		}
+
+		if (img) {
+			this._selectedImg = img;
+			this._selectedImg.style.border = '2px solid';
+			this._selectedImg.style.borderColor = 'white';
 		}
 	}
 
 	_onClickSlides(e) {
 		if (e.target && e.target.localName === 'img') {
-			setTimeout(L.bind(this._onHideSlides, this, e.target), 0);
+			this._selectImg(e.target);
 		}
 
 		e.stopPropagation();
@@ -1042,6 +1060,14 @@ class PresenterConsole {
 				notes.toLowerCase() !== 'click to add notes'.toLowerCase()
 			)
 				notesContentElem.innerText = notes;
+		}
+
+		elem = this._slides.querySelector('#slides-preview');
+		if (elem) {
+			elem = elem.children.item(this._currentIndex);
+			if (elem) {
+				this._selectImg(elem);
+			}
 		}
 
 		let next =
