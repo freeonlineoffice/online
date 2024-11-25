5fd1f1bc73f125afc69e5512ec454056a6977c88
Rashesh <rashesh.padia@collabora.com>
2024-11-25 20:57:29 +0530

    wsd: fix: don't start remoteassetconfig_poll if configkey is empty
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: I3db0479faf521d1d89f59b40b7c89ea1b2485cb8

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -1341,7 +1341,7 @@ void LOOLWSD::innerInitialize(Poco::Util::Application& self)
         { "extra_export_formats.impress_png", "false" },
         { "extra_export_formats.impress_svg", "false" },
         { "extra_export_formats.impress_tiff", "false" },
-        { "remote_template_config.url", ""},
+        { "remote_asset_config.url", ""},
         { "remote_font_config.url", ""},
     };
 
@@ -3685,16 +3685,20 @@ int LOOLWSD::innerMain()
 
     // Start the remote asset downloading polling thread.
     std::unique_ptr<RemoteAssetConfigPoll> remoteAssetConfigThread;
-    try
+    if (!uriConfigKey.empty())
     {
-        // Fetch font and/or templates settings from server if configured
-        remoteAssetConfigThread = std::make_unique<RemoteAssetConfigPoll>(config(), uriConfigKey);
-        remoteAssetConfigThread->start();
+        try
+        {
+            // Fetch font and/or templates settings from server if configured
+            remoteAssetConfigThread = std::make_unique<RemoteAssetConfigPoll>(config(), uriConfigKey);
+            remoteAssetConfigThread->start();
+        }
+        catch (const Poco::Exception&)
+        {
+            LOG_DBG("No remote_asset_config");
+        }
     }
-    catch (const Poco::Exception&)
-    {
-        LOG_DBG("No remote_asset_config");
-    }
+
 #endif
 
     // URI with /contents are public and we don't need to anonymize them.
