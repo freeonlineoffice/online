e7a61bd9da18d34a7999a36e69d2ca1164360cf1
Marco Cecchetti <marco.cecchetti@collabora.com>
2024-12-05 17:30:01 +0100

    presenter console: close windows on document reload
    Signed-off-by: Marco Cecchetti <marco.cecchetti@collabora.com>
    Change-Id: I9a42b2b772441df93b1632e1ad318a622b193153

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -226,6 +226,10 @@ class PresenterConsole {
 				'unload',
 				L.bind(this._onWindowClose, this),
 			);
+			window.addEventListener(
+				'beforeunload',
+				L.bind(this._onWindowClose, this),
+			);
 		}
 		this._proxyPresenter.addEventListener(
 			'unload',
@@ -1118,6 +1122,11 @@ class PresenterConsole {
 		if (this._proxyPresenter && !this._proxyPresenter.closed)
 			this._proxyPresenter.close();
 
+		window.removeEventListener(
+			'beforeunload',
+			L.bind(this._onWindowClose, this),
+		);
+
 		this._presenter._stopFullScreen();
 	}
 
@@ -1126,7 +1135,7 @@ class PresenterConsole {
 			this._presenter._slideShowWindowProxy &&
 			!this._presenter._slideShowWindowProxy.closed
 		)
-			this._presenter._slideShowWindowProxy.close();
+			this._presenter.slideshowWindowCleanUp();
 
 		this._proxyPresenter.removeEventListener(
 			'resize',
