570b73f53ee966ca65a8314b4112eebcefe61484
Henry Castro <hcastro@collabora.com>
2024-12-12 07:11:34 -0400

    browser: slideshow: preview repeat duration to presenter console
    Adapt patch to LibreOffice Online.
    
    Change-Id: Ie36785314c17d145008b2fd4952efc649af08a80
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -197,6 +197,12 @@ class SlideShowPresenter {
 			: 0;
 	}
 
+	_getRepeatDuration() {
+		return this._presentationInfo
+			? this._presentationInfo.loopAndRepeatDuration
+			: 0;
+	}
+
 	public isSlideHidden(slideNumber: number) {
 		const slideInfo = this.getSlideInfo(slideNumber);
 		return slideInfo ? slideInfo.hidden : true;
