3253a16063ecfa632baa5720e9bbafae004d473a
Miklos Vajna <vmiklos@collabora.com>
2024-12-17 10:51:50 +0100

    cool#10630 doc electronic sign: default to a redirect-based provider
    The old default was an in-context provider, which is the minority; set a
    new default which is closer to what will be presumably used in practice
    to give it more testing.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: I480954f9d68246fb0b5417a87fd5a6874d6890e2

--- browser/src/control/Control.ESignature.ts
+++ browser/src/control/Control.ESignature.ts
@@ -382,8 +382,10 @@ namespace lool {
 		createProviders(
 			providerIds: Array<string>,
 		): Array<lool.SignatureProvider> {
-			// Set the only tested provider as preferred.
-			const preferred = 'smart-id-signature';
+			// Providers can be in-context or redirect-based.  Most real-world providers
+			// are redirect-based, set the only tested non-in-context provider as
+			// preferred.
+			const preferred = 'd-trust-sign-me-qes-signature';
 			const index = providerIds.indexOf(preferred);
 			if (index != -1) {
 				providerIds.splice(index, /*deleteCount=*/ 1);
@@ -419,6 +421,14 @@ namespace lool {
 			}
 			codes = [...new Set(codes)].sort();
 			this.availableCountryCodes = codes;
+			// Set the country for the default provider as preferred.
+			const preferred = 'DE';
+			const index = codes.indexOf(preferred);
+			if (index != -1) {
+				codes.splice(index, /*deleteCount=*/ 1);
+				codes.splice(/*start=*/ 0, /*deleteCount=*/ 0, preferred);
+			}
+
 			return codes.map((code) => {
 				const countryName = ESignature.countryNames[code];
 				if (countryName) {
