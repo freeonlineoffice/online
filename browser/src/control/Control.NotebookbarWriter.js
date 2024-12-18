ed3497c89e74fdf0c10d4c08ddfaa51ca35e1d55
Szymon Kłos <szymon.klos@collabora.com>
2024-12-18 14:23:58 +0100

    style list: make experimental
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I00af2745fc4bfa17bc4b4f5dae83f387e65611b8

--- browser/src/control/Control.NotebookbarWriter.js
+++ browser/src/control/Control.NotebookbarWriter.js
@@ -3,7 +3,7 @@
  * L.Control.NotebookbarWriter
  */
 
-/* global _ _UNO */
+/* global _ _UNO app */
 
 var fileTabName = 'File';
 var homeTabName = 'Home';
@@ -923,13 +923,13 @@ L.Control.NotebookbarWriter = L.Control.Notebookbar.extend({
 				'command': '.uno:ParagraphDialog',
 				'accessibility': { focusBack: false, combination: 'B', de: null }
 			},
-			{
+			app.isExperimentalMode() ? {
 				'id': 'format-style-dialog',
 				'type': 'bigtoolitem',
-				'text': _UNO('.uno:SidebarDeck.StyleListDeck'),
+				'text': _('Style list'),
 				'command': '.uno:SidebarDeck.StyleListDeck',
 				'accessibility': { focusBack: false, combination: 'SD', de: null }
-			},
+			} : {},
 			{
 				'id': 'format-FormatBulletsMenu',
 				'type': 'menubutton',
