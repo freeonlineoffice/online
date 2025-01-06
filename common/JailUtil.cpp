54ebf3e1fe2b58db417524373155e54e90f07f5e
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-01-05 21:38:53 -0500

    wsd: rename right-trimming helper to rtrim
    Change-Id: Ia9a7a6a856b09d8679fa46ee2dd3dde685baad75
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- common/JailUtil.cpp
+++ common/JailUtil.cpp
@@ -117,8 +117,8 @@ bool enterUserNS(uid_t uid, gid_t gid)
 
 bool loolmount(const std::string& arg, std::string source, std::string target, bool silent = false)
 {
-    source = Util::trim(source, '/');
-    target = Util::trim(target, '/');
+    source = Util::rtrim(source, '/');
+    target = Util::rtrim(target, '/');
 
     if (isMountNamespacesEnabled())
     {
