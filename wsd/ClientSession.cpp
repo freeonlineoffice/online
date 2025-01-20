918574eae00f817b14f5f9b392a9d51d2fb5d757
Rashesh <rashesh.padia@collabora.com>
2025-01-20 22:11:30 +0530

    presets: stop uploading browsersettings unnecessarily
    - we don't need to upload for each session which matches the userId
    
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: I46cfcc0877e9a178a0028d1e7249e838a8260d7d

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -1414,11 +1414,9 @@ void ClientSession::uploadBrowserSettingsToWopiHost(const std::string& docKey)
     }
 }
 
-void ClientSession::updateBrowserSettingsJSON(const std::string& key, const std::string& value,
-                                              const std::string& docKey)
+void ClientSession::updateBrowserSettingsJSON(const std::string& key, const std::string& value)
 {
     std::vector<std::string> vec = Util::splitStringToVector(key, '.');
-
     if (vec.size() == 2)
     {
         const std::string& parentKey = vec[0];
@@ -1439,13 +1437,6 @@ void ClientSession::updateBrowserSettingsJSON(const std::string& key, const std:
     {
         _browserSettingsJSON->set(key, value);
     }
-
-    {
-        std::ostringstream jsonStream;
-        _browserSettingsJSON->stringify(jsonStream, 2);
-    }
-
-    uploadBrowserSettingsToWopiHost(Uri::decode(docKey));
 }
 
 #endif
