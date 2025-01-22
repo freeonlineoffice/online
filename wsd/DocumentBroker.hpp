d7f011a128c0d3419779d9c2abe53cae0f0239ce
Rashesh <rashesh.padia@collabora.com>
2025-01-22 17:03:22 +0530

    presets: added comment
    Signed-off-by: Rashesh <rashesh.padia@collabora.com>
    Change-Id: I7dcbc9263b8f388b942981cd5fbc19efa78d36f5

--- wsd/DocumentBroker.hpp
+++ wsd/DocumentBroker.hpp
@@ -1729,6 +1729,7 @@ private:
 
 #if !MOBILEAPP
     Admin& _admin;
+    /// stores timestamps of preset files when they get installed to compare later to check if they are modified
     std::map<std::string, std::filesystem::file_time_type> _presetTimestamp;
 #endif
 
