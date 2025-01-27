ac8585a59bfb32795b440cde26afd2e55af14d1d
Miklos Vajna <vmiklos@collabora.com>
2025-01-27 08:34:23 +0100

    cool#10630 doc electronic sign: fix outdated tile for shape move on 2nd page
    Open a PDF with 2 pages, go to the second page, insert -> signature
    line, drag the graphic selection, the shape doesn't show up at the new
    location.
    
    Seems this is really similar to the problem when inserting a signature
    line doesn't invalidate properly.
    
    There is still no precise tracking of invalidation for read-only &
    multi-page PDF files, so just extract Control.Menubar.js code to a new
    requestNewFiledBasedViewTiles() and use that in
    ShapeHandlesSection.sendTransformCommand().
    
    With this, visual esign for non-first pages is about as usable as for
    first pages, finally.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Change-Id: Idbf0cfe32030471eb0472ff9a00ccc0304cc4cf3

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -3819,6 +3819,18 @@ L.CanvasTileLayer = L.Layer.extend({
 		return verticalOffset;
 	},
 
+	// If viewing multi-page PDF files, no precise tracking of invalidations is implemented yet,
+	// so this allows requesting new tiles when we know a viewed PDF changes for some special
+	// reason.
+	requestNewFiledBasedViewTiles: function() {
+		if (!app.file.fileBasedView) {
+			return;
+		}
+
+		this._requestNewTiles();
+		this.redraw();
+	},
+
 	// Given a character code and a UNO keycode, send a "key" message to loolwsd.
 	//
 	// "type" is either "input" for key presses (akin to the DOM "keypress"
