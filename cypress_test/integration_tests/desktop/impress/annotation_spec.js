df53f941e6e98a9fddb20cabe7255066996e38bc
Gökay Şatır <gokaysatir@collabora.com>
2025-02-05 14:54:23 +0300

    Fix typo.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: I472290f30c5c71f71fa22d40245db50ce39dabfc

--- cypress_test/integration_tests/desktop/impress/annotation_spec.js
+++ cypress_test/integration_tests/desktop/impress/annotation_spec.js
@@ -59,7 +59,7 @@ describe(['tagdesktop'], 'Annotation Tests', function() {
 		cy.cGet('.lool-annotation-content > div').should('include.text','some reply text');
 	});
 
-	it('Tab Nevigation', function() {
+	it('Tab Navigation', function() {
 		desktopHelper.insertComment(undefined, false);
 
 		cy.cGet('.lool-annotation-autosavelabel').should('be.not.visible');
