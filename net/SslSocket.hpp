346f3d8102903d086bdf171e6af9623eb04a798e
Méven Car <meven.car@collabora.com>
2025-02-14 10:09:41 +0100

    net: minimize size of enum SslWantsTo
    Signed-off-by: Méven Car <meven.car@collabora.com>
    Change-Id: If2033335dc16b1192b57120a2f73b0625876c87f

--- net/SslSocket.hpp
+++ net/SslSocket.hpp
@@ -235,7 +235,7 @@ public:
 
 private:
     /// The possible next I/O operation that SSL want to do.
-    enum class SslWantsTo
+    enum class SslWantsTo : std::uint8_t
     {
         Neither,
         Read,
