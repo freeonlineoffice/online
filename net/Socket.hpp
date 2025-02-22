291d5485cca8794f8f0af6b42f870f8e2451bc5c
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-02-22 10:05:41 -0500

    wsd: remove single-use setClosed()
    Change-Id: I7acf1c52cc36a1a4b758f50d7cf218eaaefc597a
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- net/Socket.hpp
+++ net/Socket.hpp
@@ -454,9 +454,6 @@ protected:
     /// Explicitly marks this socket closed, i.e. rejected from polling and potentially shutdown
     void setClosed() { _closed = true; }
 
-    /// Explicitly marks this socket and the given SocketDisposition closed
-    void setClosed(SocketDisposition &disposition) { setClosed(); disposition.setClosed(); }
-
 private:
     /// Create socket of the given type.
     /// return >= 0 for a successfully created socket, -1 on error
@@ -1594,7 +1591,8 @@ public:
         {
             LOG_TRC("Closed. Firing onDisconnect.");
             _socketHandler->onDisconnect();
-            setClosed(disposition);
+            setClosed();
+            disposition.setClosed();
         }
         else if (isClosed())
             disposition.setClosed();
