757abc7b1c0ce99674d9919be52df6830eb3f5bd
Banobe Pascal <banobe.pascal@collabora.com>
2025-02-24 01:20:29 +0300

    status bar: hide on/off indicators if they have default values
    Add data-default attribute to status elements to hide indicators when
    they are in default state:
    
     - InsertMode when the value is 'Insert'
     - showcomments when the value is 'Comments: On'
     - permissionmode when the value is 'Edit mode'
     - StatusSelectionMode when the value is 'Standard selection'
    
    Signed-off-by: Banobe Pascal <banobe.pascal@collabora.com>
    Change-Id: I4198cf50361a917800c3eab144d31e3ffcb2aa1a

--- browser/src/control/Control.StatusBar.js
+++ browser/src/control/Control.StatusBar.js
@@ -175,6 +175,7 @@ class StatusBar extends JSDialog.Toolbar {
 			statemsg = _UNO('.uno:ShowAnnotations') +': ' + _('On');
 		else if (state === 'false')
 			statemsg = _UNO('.uno:ShowAnnotations') +': ' + _('Off');
+		$('#showcomments-container').attr('default-state', state || null);
 		this.updateHtmlItem('ShowComments', state ? statemsg : ' ');
 	}
 
@@ -465,6 +466,7 @@ class StatusBar extends JSDialog.Toolbar {
 
 			$('#InsertMode').removeClass();
 			$('#InsertMode').addClass('jsdialog ui-badge insert-mode-' + state);
+			$('#insertmode-container').attr('default-state', state || null);
 
 			if ((state === 'false' || !state) && app.definitions.urlPopUpSection.isOpen()) {
 				this.map.hyperlinkUnderCursor = null;
@@ -472,6 +474,7 @@ class StatusBar extends JSDialog.Toolbar {
 			}
 		}
 		else if (commandName === '.uno:StatusSelectionMode' || commandName === '.uno:SelectionMode') {
+			$('#statusselectionmode-container').attr('default-state', state === '0' || null);
 			this.updateHtmlItem('StatusSelectionMode', state ? L.Styles.selectionMode[state].toLocaleString() : _('Selection mode: inactive'), !state);
 		}
 		else if (commandName == '.uno:StateTableCell') {
@@ -515,6 +518,7 @@ class StatusBar extends JSDialog.Toolbar {
 		}
 		else if (commandName === '.uno:EditDoc') {
 			state = state !== "false";
+			$('#permissionmode-container').attr('default-state', this.map.isEditMode() || null);
 			this.onPermissionChanged({detail : {
 				perm: state && this.map.isEditMode() ? "edit" : "readonly"
 			} });
