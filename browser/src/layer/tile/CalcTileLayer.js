7fa45f876e76ec62ba5e281dd03238fba9abd607
Pranam Lashkari <lpranam@collabora.com>
2025-02-26 00:58:02 +0530

    comment: use iso date strings everywhere
    also display time in comments
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: I3c98a02bec018560daceb3b0350f1cc95d532396

--- browser/src/layer/tile/CalcTileLayer.js
+++ browser/src/layer/tile/CalcTileLayer.js
@@ -49,7 +49,7 @@ L.CalcTileLayer = L.CanvasTileLayer.extend({
 				anchorPos: app.calc.cellCursorRectangle.toArray(),
 				id: 'new',
 				tab: this._selectedPart,
-				dateTime: new Date().toDateString(),
+				dateTime: new Date().toISOString(),
 				author: this._map.getViewName(this._viewId)
 			};
 
