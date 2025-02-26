c5745c5f9b04ae038335018429f3eb630d4339fc
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-02-26 17:38:53 +0000

    delegating constructor to easily allow rearranging to remove hole
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ia1a7800db425c1220c4456e9588d44d4a7decebf

--- wsd/TraceFile.hpp
+++ wsd/TraceFile.hpp
@@ -355,17 +355,9 @@ private:
 class TraceFileReader
 {
 public:
-    TraceFileReader(const std::string& path) :
-        _compressed(path.size() > 2 && path.substr(path.size() - 2) == "gz"),
-        _epochStart(0),
-        _epochEnd(0),
-        _stream(path, _compressed ? std::ios::binary : std::ios::in),
-        _inflater(_stream, Poco::InflatingStreamBuf::STREAM_GZIP),
-        _index(0),
-        _indexIn(-1),
-        _indexOut(-1)
+    TraceFileReader(const std::string& path)
+        : TraceFileReader(path, (path.size() > 2 && path.substr(path.size() - 2) == "gz"))
     {
-        readFile();
     }
 
     ~TraceFileReader()
@@ -413,6 +405,19 @@ public:
     }
 
 private:
+    TraceFileReader(const std::string& path, bool compressed) :
+        _stream(path, compressed ? std::ios::binary : std::ios::in),
+        _inflater(_stream, Poco::InflatingStreamBuf::STREAM_GZIP),
+        _epochStart(0),
+        _epochEnd(0),
+        _index(0),
+        _indexIn(-1),
+        _indexOut(-1),
+        _compressed(compressed)
+    {
+        readFile();
+    }
+
     void readFile()
     {
         _records.clear();
@@ -517,15 +522,15 @@ private:
     }
 
 private:
-    const bool _compressed;
-    int64_t _epochStart;
-    int64_t _epochEnd;
     std::ifstream _stream;
     Poco::InflatingInputStream _inflater;
     std::vector<TraceFileRecord> _records;
+    int64_t _epochStart;
+    int64_t _epochEnd;
     unsigned _index;
     unsigned _indexIn;
     unsigned _indexOut;
+    const bool _compressed;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
