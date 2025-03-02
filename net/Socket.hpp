e120a7aae9f02ad63c30105d7c564fdbd557ee8c
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-03-02 17:44:19 +0000

    cid#318975 silence Out-of-bounds access
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ib0c888ece329c84d1a16eb3855fcf6a363b9d119

--- net/Socket.hpp
+++ net/Socket.hpp
@@ -1307,8 +1307,7 @@ public:
 
                 if (len > 0)
                 {
-                    LOG_ASSERT_MSG(len <= ssize_t(sizeof(buf)),
-                                   "Read more data than the buffer size");
+                    assert(len <= ssize_t(sizeof(buf)) && "Read more data than the buffer size");
                     notifyBytesRcvd(len);
                     _inBuffer.append(&buf[0], len);
                 }
