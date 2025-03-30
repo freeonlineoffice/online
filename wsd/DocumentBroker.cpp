a365a03d83e3e26d76dc1d82a0566d46a6736986
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-03-30 20:26:36 +0100

    cid#512065 Variable copied when it could be moved
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I6cae4c3a061130c12643990fafc5472fd725c3bf

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -2623,9 +2623,8 @@ void DocumentBroker::uploadToStorageInternal(const std::shared_ptr<ClientSession
     const std::string uriAnonym = LOOLWSD::anonymizeUrl(uri);
 
     // If the file timestamp hasn't changed, skip uploading.
-    const std::string filePath = _storage->getRootFilePathUploading();
     const std::chrono::system_clock::time_point newFileModifiedTime
-        = FileUtil::Stat(filePath).modifiedTimepoint();
+        = FileUtil::Stat(_storage->getRootFilePathUploading()).modifiedTimepoint();
     if (!isSaveAs && newFileModifiedTime == _saveManager.getLastModifiedTime() && !isRename
         && !force)
     {
