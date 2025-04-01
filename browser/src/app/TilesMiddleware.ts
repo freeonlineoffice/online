f3eb5f0216a4d4819eab216dc27f991f4b2ba4ae
Chris Lord <chris.lord@collabora.com>
2025-04-01 12:34:18 +0100

    Add Bounds.distanceTo(Bounds) and use for tile distance
    Add a function to Bounds that gives the minimum distance that would cause
    another set of bounds to intersect with it. Use this new function for
    tile distance. This causes GC to better respect the shape of the visible
    bounds.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Chris Lord <chris.lord@collabora.com>
    Change-Id: I01f045cf405515c25975398170cf1d56b0905d22

--- browser/src/app/TilesMiddleware.ts
+++ browser/src/app/TilesMiddleware.ts
@@ -1519,19 +1519,9 @@ class TileManager {
 				tile.coords.y + this.tileSize,
 			),
 		);
-		if (tileBounds.intersectsAny(visibleRanges)) {
-			tile.distanceFromView = 0;
-			return;
-		}
-
-		const tileCenter = tileBounds.getCenter();
-		tile.distanceFromView = tileCenter.distanceTo(
-			visibleRanges[0].getCenter(),
-		);
+		tile.distanceFromView = tileBounds.distanceTo(visibleRanges[0]);
 		for (let i = 1; i < visibleRanges.length; ++i) {
-			const distance = tileCenter.distanceTo(
-				visibleRanges[i].getCenter(),
-			);
+			const distance = tileBounds.distanceTo(visibleRanges[i]);
 			if (distance < tile.distanceFromView)
 				tile.distanceFromView = distance;
 		}
