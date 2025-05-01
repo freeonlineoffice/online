2e0b309904d91c1162645c85bab738ea602f200c
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-05-01 17:41:10 +0100

    split out socketEraseConsumedBytes as a function
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I0f5f2c3fd9439cb5a2ba5c8b26ef32b5adc3b795

--- wsd/ClientRequestDispatcher.cpp
+++ wsd/ClientRequestDispatcher.cpp
@@ -707,6 +707,22 @@ void launchAsyncCheckFileInfo(
     }
 }
 
+static void socketEraseConsumedBytes(const std::shared_ptr<StreamSocket>& socket,
+                                     const StreamSocket::MessageMap& map,
+                                     bool servedSync)
+{
+    if( socket->getInBuffer().size() > 0 ) // erase request from inBuffer if not cleared by ignoreInput
+    {
+        // Remove the request header from our input buffer
+        socket->eraseFirstInputBytes(map._headerSize);
+        if (servedSync)
+        {
+            // Remove the request body from our input buffer, as it has been served (synchronously)
+            // See lool#9621, commit 895c224efae9c21f0481e2fbf024a015656a5a97 and lool#10042
+            socket->eraseFirstInputBytes(map._messageSize - map._headerSize);
+        }
+    }
+}
 
 void ClientRequestDispatcher::handleIncomingMessage(SocketDisposition& disposition)
 {
@@ -1028,16 +1044,8 @@ void ClientRequestDispatcher::handleIncomingMessage(SocketDisposition& dispositi
         return;
     }
 
-    if( socket->getInBuffer().size() > 0 ) // erase request from inBuffer if not cleared by ignoreInput
-    {
-        // Remove the request header from our input buffer
-        socket->eraseFirstInputBytes(map._headerSize);
-        if (servedSync)
-        {
-            // Remove the request body from our input buffer, as it has been served (synchronously)
-            socket->eraseFirstInputBytes(map._messageSize - map._headerSize);
-        }
-    }
+
+    socketEraseConsumedBytes(socket, map, servedSync);
 
     if (servedSync && closeConnection && !socket->isShutdown())
     {
