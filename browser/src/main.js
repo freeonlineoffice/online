2d19243b0b69990062635e6a7be233140d2cdae6
Stephan Bergmann <stephan.bergmann@allotropia.de>
2025-06-12 17:30:51 +0200

    Revive COWASM: createOnlineModule returns a promise
    ...so the Module slots read here are not necessarily already set by the time the
    call to crateOnlineModule returns.  (TODO: But it's probably just good luck if
    the promise resolves and the app slots are set in time before their first use?)
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
    Change-Id: I59adee337c0057c44fdd7ef52f315d580a26cc11

--- browser/src/main.js
+++ browser/src/main.js
@@ -114,9 +114,9 @@ if (window.ThisIsTheEmscriptenApp) {
 		arguments_: [docURL, encodedWOPI, isWopi ? 'true' : 'false'],
 		arguments: [docURL, encodedWOPI, isWopi ? 'true' : 'false'],
 	};
-	createOnlineModule(Module);
-	app.HandleLOOLMessage = Module['_handle_lool_message'];
-	app.AllocateUTF8 = Module['allocateUTF8'];
+	createOnlineModule(Module).then(() => {
+		app.HandleLOOLMessage = Module['_handle_lool_message'];
+		app.AllocateUTF8 = Module['allocateUTF8']; });
 } else {
 	map.loadDocument(global.socket);
 }
