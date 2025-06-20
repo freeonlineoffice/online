74d28210df2de5ee805617e47dd108d6c374098f
Szymon Kłos <szymon.klos@collabora.com>
2025-06-20 11:06:53 +0000

    sidebar: simplify focus on open
    followup for commit ed25a3af8718f35b68309d7afe9de8748eeb065a
    browser: accessibility: fire the 'jsdialogclick' to perform post-processing.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: Ib082c25ee61207a134e261dc765e1a76822a18cc

--- browser/src/control/Control.Sidebar.ts
+++ browser/src/control/Control.Sidebar.ts
@@ -27,7 +27,6 @@ class Sidebar extends SidebarBase {
 	onAdd(map: ReturnType<typeof L.map>) {
 		super.onAdd(map);
 		this.map.on('sidebar', this.onSidebar, this);
-		this.map.on('jsdialogclick', this.onJSDialogClick, this);
 	}
 
 	onRemove() {
@@ -81,12 +80,6 @@ class Sidebar extends SidebarBase {
 		if (unoCommand !== null && unoCommand !== undefined)
 			app.socket.sendMessage('uno ' + unoCommand);
 		this.setupTargetDeck(unoCommand);
-	}
-
-	onJSDialogClick(e: any) {
-		if (e.uno === '.uno:SidebarDeck.PropertyDeck') {
-			this.enableFocus = true;
-		}
 	}
 
 	onSidebar(data: FireEvent) {
@@ -148,17 +141,14 @@ class Sidebar extends SidebarBase {
 				}
 
 				this.builder.build(this.container, [sidebarData]);
-				if (!this.isVisible())
+				if (!this.isVisible()) {
 					$('#sidebar-dock-wrapper').addClass('visible');
-
-				if (this.enableFocus === true) {
 					const focusables = JSDialog.GetFocusableElements(
 						this.container,
 					);
 					if (focusables && focusables.length) {
 						focusables[0].focus();
 					}
-					this.enableFocus = false;
 				}
 
 				this.map.uiManager.setDocTypePref('ShowSidebar', true);
