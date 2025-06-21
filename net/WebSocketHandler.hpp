f5697fe86e4e40a1ebcd7707b2f0a10762f696ab
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-06-21 08:15:00 -0400

    wsd: test: better logging
    Change-Id: Ifdf66844c7f761858fe7c7586477fb5d7dee5626
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- net/WebSocketHandler.hpp
+++ net/WebSocketHandler.hpp
@@ -152,6 +152,8 @@ public:
             return false;
         }
 
+        LOGA_TRC(WebSocket,
+                 "Connected #" << socket->getFD() << " to " << hostAndPort << ", sending request");
         onConnect(socket);
 
         req.set("Host", hostAndPort); // Make sure the host is set.
