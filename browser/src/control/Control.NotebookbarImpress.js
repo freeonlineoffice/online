63b5441b4c7c2c62789a9576a6a7f532aff3c005
Mohit Marathe <mohit.marathe@collabora.com>
2025-07-04 18:06:48 +0530

    Impress: place Slide Show tab after Layout tab
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I76be8f661476e5dc03a0171b00bddeabff2bf13f

--- browser/src/control/Control.NotebookbarImpress.js
+++ browser/src/control/Control.NotebookbarImpress.js
@@ -101,6 +101,12 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 				'accessibility': { focusBack: false, combination: 'P', de: null }
 			},
 			{
+				'id': 'Slideshow-tab-label',
+				'text': _('Slide Show'),
+				'name': 'Slideshow',
+				'accessibility': { focusBack: false, combination: 'SE', de: null }
+			},
+			{
 				'id': 'Review-tab-label',
 				'text': _('Review'),
 				'name': 'Review',
@@ -141,12 +147,6 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 				'accessibility': { focusBack: false, combination: 'M', de: null }
 			},
 			{
-				'id': 'Slideshow-tab-label',
-				'text': _('Slide Show'),
-				'name': 'Slideshow',
-				'accessibility': { focusBack: false, combination: 'SE', de: null }
-			},
-			{
 				'id': 'View-tab-label',
 				'text': _('View'),
 				'name': 'View',
@@ -167,13 +167,13 @@ L.Control.NotebookbarImpress = L.Control.NotebookbarWriter.extend({
 			this.getHomeTab(),
 			this.getInsertTab(),
 			this.getLayoutTab(),
+			this.getSlideshowTab(),
 			this.getReviewTab(),
 			this.getFormatTab(),
 			this.getTableTab(),
 			this.getShapeTab(),
 			this.getPictureTab(),
 			this.getMasterTab(),
-			this.getSlideshowTab(),
 			this.getViewTab(),
 			this.getHelpTab()
 		];
