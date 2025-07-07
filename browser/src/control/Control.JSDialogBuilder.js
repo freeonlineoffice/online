4d507c455841ac0c6c03d23581ec746b35a8ec11
Henry Castro <hcastro@collabora.com>
2025-07-07 09:02:59 -0400

    browser: fix the split dropdown button’s aria-expanded attribute.
    Change-Id: Ic33a2ade6959249afa50672f8bcd31b710292a78
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/control/Control.JSDialogBuilder.js
+++ browser/src/control/Control.JSDialogBuilder.js
@@ -2276,7 +2276,6 @@ L.Control.JSDialogBuilder = L.Control.extend({
 				});
 
 				div.closeDropdown = function() {
-					arrowbackground.setAttribute('aria-expanded', false);
 					builder.callback('toolbox', 'closemenu', parentContainer, data.command, builder);
 				};
 			}
@@ -2298,9 +2297,15 @@ L.Control.JSDialogBuilder = L.Control.extend({
 			}
 		}
 
+		div._onDropDown = function(open) {
+			// Only set aria-expanded on the button if the arrow is not interactive
+			if (!isArrowInteractive)
+				button.setAttribute('aria-expanded', open);
+			arrowbackground.setAttribute('aria-expanded', open);
+		};
+
 		var openToolBoxMenu = function(event) {
 			if (!div.hasAttribute('disabled')) {
-				arrowbackground.setAttribute('aria-expanded', true);
 				builder.callback('toolbox', 'openmenu', parentContainer, data.command, builder);
 				event.stopPropagation();
 			}
@@ -2315,9 +2320,6 @@ L.Control.JSDialogBuilder = L.Control.extend({
 					builder.callback('toolbutton', 'click', button, data.command, builder);
 				else {
 					builder.callback('toolbox', 'click', parentContainer, data.command, builder);
-					// Only set aria-expanded on the button if the arrow is not interactive
-					if (!isArrowInteractive)
-						button.setAttribute('aria-expanded', true);
 				}
 			}
 			e.preventDefault();
