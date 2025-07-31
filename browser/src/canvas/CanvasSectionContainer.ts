6852de4cbacc0b573e5c809634e24433a34fdb82
andreasma <maand@gmx.de>
2025-07-31 21:08:32 +0200

    Removed unused @ts-expect-error directives
    Signed-off-by: andreasma <maand@gmx.de>
    Change-Id: Idb07a53f20ec8bb8816fe1c7c221b66dbc327427

--- browser/src/canvas/CanvasSectionContainer.ts
+++ browser/src/canvas/CanvasSectionContainer.ts
@@ -1538,7 +1538,6 @@ class CanvasSectionContainer {
 
 
 	private calculateSectionInitialPosition(section: CanvasSectionObject, index: number): number {
-		// @ts-expect-error because alternative got type 'never'.
 		if (typeof section.anchor[index] === 'string' || section.anchor[index].length === 1) {
 			const anchor: string | string[] = typeof section.anchor[index] === 'string' ? section.anchor[index]: section.anchor[index][0];
 			if (index === 0)
@@ -1551,13 +1550,11 @@ class CanvasSectionContainer {
 			// Count should always be an odd number. Because last variable will be used as a fallback to canvas's edges (top, bottom, right or left).
 			// See anchor explanation on top of this file.
 			// Correct example: ["header", "bottom", "top"] => Look for section "header", if found, use its bottom, if not found, use canvas's top.
-			// @ts-expect-error because type never has no attribute length.
 			if (section.anchor[index].length % 2 === 0) { // eslint-disable-line no-lonely-if
 				console.error('Section: ' + section.name + '. Wrong anchor definition.');
 				return 0;
 			}
 			else {
-				// @ts-expect-error because type never has no attribute length.
 				var count: number = section.anchor[index].length;
 				var targetSection: CanvasSectionObject = null;
 				var targetEdge: string = null;
