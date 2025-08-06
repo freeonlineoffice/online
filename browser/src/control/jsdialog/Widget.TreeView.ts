72525fb218c09cfbc579ae169d71ad702c21cc28
NickWingate <nick.wingate@collabora.com>
2025-08-06 13:57:27 +0100

    Navigation: Created unifed search bar
    When the user searches we populate both
    the QuickFind (now Results) tab and highlight
    hits on the Outline tab.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: NickWingate <nick.wingate@collabora.com>
    Change-Id: I19cb0f8c0fe16aced901776e31b5e548dd19555d

--- browser/src/control/jsdialog/Widget.TreeView.ts
+++ browser/src/control/jsdialog/Widget.TreeView.ts
@@ -1137,6 +1137,43 @@ class TreeViewControl {
 		}, 100);
 	}
 
+	highlightEntries(searchTerm: string) {
+		if (this._filterTimer) clearTimeout(this._filterTimer);
+		var entriesToHighlight: Array<HTMLElement> = [];
+		var allEntries =
+			this._container.querySelectorAll('.ui-treeview-entry');
+
+		searchTerm = searchTerm.trim();
+
+		allEntries.forEach((entry: HTMLElement) => {
+			if (searchTerm === '') return;
+
+			var cells = entry.querySelectorAll('div');
+			for (var i in cells) {
+				var entryText = cells[i].innerText;
+				if (
+					entryText &&
+					entryText
+						.toLowerCase()
+						.indexOf(searchTerm.toLowerCase()) >= 0
+				) {
+					entriesToHighlight.push(entry);
+				}
+			}
+
+			return;
+		});
+
+		this._filterTimer = setTimeout(() => {
+			allEntries.forEach((entry) => {
+				L.DomUtil.removeClass(entry, 'highlighted');
+			});
+			entriesToHighlight.forEach((entry) => {
+				L.DomUtil.addClass(entry, 'highlighted');
+			});
+		}, 100);
+	}
+
 	setupKeyEvents(data: TreeWidgetJSON, builder: JSBuilder) {
 		this._container.addEventListener('keydown', (event) => {
 			const listElements =
@@ -1618,6 +1655,8 @@ class TreeViewControl {
 		this._tbody = this._container;
 		(this._container as any).filterEntries =
 			this.filterEntries.bind(this);
+		(this._container as any).highlightEntries =
+			this.highlightEntries.bind(this);
 
 		this.setupDragAndDrop(data, builder);
 		this.setupKeyEvents(data, builder);
