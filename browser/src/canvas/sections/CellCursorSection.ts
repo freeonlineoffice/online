a26fdabb205488a1db4264e7b54b13f62a3babcf
andreasma <maand@gmx.de>
2025-08-14 21:50:50 +0200

    Naming fixes
    Signed-off-by: andreasma <maand@gmx.de>
    Change-Id: I505c7337ae37add06989d1071f75d9063b16d72e

--- browser/src/canvas/sections/CellCursorSection.ts
+++ browser/src/canvas/sections/CellCursorSection.ts
@@ -88,7 +88,7 @@ class CellCursorSection extends CanvasSectionObject {
 		}
 	}
 
-	public onMouseEnter(point: cool.SimplePoint, e: MouseEvent): void {
+	public onMouseEnter(point: lool.SimplePoint, e: MouseEvent): void {
 		this.sectionProperties.mouseInside = true;
 		if (!app.file.textCursor.visible) return;
 		const grid: any = document.querySelector('.leaflet-layer');
@@ -96,14 +96,14 @@ class CellCursorSection extends CanvasSectionObject {
 		grid.style.cursor = 'text';
 	}
 
-	public onMouseLeave(point: cool.SimplePoint, e: MouseEvent): void {
+	public onMouseLeave(point: lool.SimplePoint, e: MouseEvent): void {
 		this.sectionProperties.mouseInside = false;
 		const grid: any = document.querySelector('.leaflet-layer');
 		grid.classList.add('spreadsheet-cursor');
 		grid.style.cursor = '';
 	}
 
-	public onClick(point: cool.SimplePoint, e: MouseEvent): void {
+	public onClick(point: lool.SimplePoint, e: MouseEvent): void {
 		this.sectionProperties.mouseInside = true;
 	}
 }
