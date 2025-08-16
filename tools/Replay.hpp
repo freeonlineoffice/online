55d99ca4aed52717a1b2f8269e68928df0307ea4
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-08-16 10:22:06 -0400

    wsd: test: convert cerr output to proper logging
    We can still output the log to the console,
    if so we wish, but at least the cerr entries
    are now consistent with the general logging
    and we can easily see timestamps, thread-id,
    and other meta-data.
    
    Change-Id: I006b657c1c3bbc6d398391f8ae37ccf9884a5dc5
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- tools/Replay.hpp
+++ tools/Replay.hpp
@@ -404,7 +404,7 @@ public:
 
         static std::atomic<int> number;
         _logPre = '[' + std::to_string(++number) + "] ";
-        std::cerr << "Attempt connect to " << uri << " for trace " << _trace << '\n';
+        LOG_TST("Attempt connect to " << uri << " for trace " << _trace);
         getNextRecord();
         _start = std::chrono::steady_clock::now() + std::chrono::milliseconds(delayMs);
         _nextPing = _start + std::chrono::milliseconds(Util::rng::getNext() % 1000);
@@ -421,8 +421,8 @@ public:
     {
         if (_connecting)
         {
-            std::cerr << _logPre << "Waiting for outbound connection to " << _uri
-                      << " to complete for trace " << _trace << '\n';
+            LOG_TST(_logPre << "Waiting for outbound connection to " << _uri
+                            << " to complete for trace " << _trace);
             return POLLOUT;
         }
 
@@ -448,7 +448,7 @@ public:
             }
         }
 
-//        std::cerr << "next event in " << nextTime << " us\n";
+        //        LOG_TST( "next event in " << nextTime << " us");
         if (nextTime < timeoutMaxMicroS)
             timeoutMaxMicroS = nextTime;
 
@@ -477,15 +477,14 @@ public:
     void performWrites(std::size_t capacity) override
     {
         if (_connecting)
-            std::cerr << _logPre << "Outbound websocket - connected\n";
+            LOG_TST(_logPre << "Outbound websocket - connected");
         _connecting = false;
         return WebSocketHandler::performWrites(capacity);
     }
 
     void onDisconnect() override
     {
-        std::cerr << _logPre << "Websocket " << _uri <<
-            " dis-connected, re-trying in 20 seconds\n";
+        LOG_TST(_logPre << "Websocket " << _uri << " dis-connected, re-trying in 20 seconds");
         WebSocketHandler::onDisconnect();
     }
 
@@ -498,13 +497,13 @@ public:
         std::string msg = rewriteMessage(_next.getPayload());
         if (!msg.empty())
         {
-            std::cerr << _logPre << "Send: '" << msg << "'\n";
+            LOG_TST(_logPre << "Send: '" << msg << "'");
             sendMessage(msg);
         }
 
         if (!getNextRecord())
         {
-            std::cerr << _logPre << "Shutdown\n";
+            LOG_TST(_logPre << "Shutdown");
             shutdown();
         }
     }
@@ -530,7 +529,7 @@ public:
             out = "load url=" + _uri; // already encoded
             for (size_t i = 2; i < tokens.size(); ++i)
                 out += ' ' + tokens[i];
-            std::cerr << _logPre << "msg " << out << '\n';
+            LOG_TST(_logPre << "msg " << out);
         }
 
         size_t currentMemoryUsage = _stats->getMemoryUsage();
@@ -553,7 +552,7 @@ public:
 
         const std::string firstLine = LOOLProtocol::getFirstLine(data.data(), data.size());
         StringVector tokens = StringVector::tokenize(firstLine);
-        std::cerr << _logPre << "Got msg: " << firstLine << '\n';
+        LOG_TST(_logPre << "Got msg: " << firstLine);
 
         _stats->accumulateRecv(tokens[0], data.size());
 
@@ -567,27 +566,26 @@ public:
             TileDesc desc = TileDesc::parse(tokens);
 
             sendMessage("tileprocessed tile=" + desc.generateID());
-            std::cerr << _logPre << "Sent tileprocessed tile= " + desc.generateID() << '\n';
+            LOG_TST(_logPre << "Sent tileprocessed tile= " + desc.generateID());
         }
         else if (tokens.equals(0, "error:"))
         {
             bool reconnect = false;
             if (firstLine == "error: cmd=load kind=docunloading")
             {
-                std::cerr << ": wait and try again later ...!\n";
+                LOG_TST(": wait and try again later ...!");
                 reconnect = true;
             }
             else if (firstLine == "error: cmd=storage kind=documentconflict")
             {
-                std::cerr << "Document conflict - need to resolve it first ...\n";
+                LOG_TST("Document conflict - need to resolve it first ...");
                 sendMessage("closedocument");
                 reconnect = true;
             }
             else
             {
-                std::cerr << _logPre << "Error while processing " << _uri
-                          << " and trace " << _trace << ":\n"
-                          << "'" << firstLine << "'\n";
+                LOG_TST(_logPre << "Error while processing " << _uri << " and trace " << _trace
+                                << ": [" << firstLine << ']');
             }
 
             if (reconnect)
