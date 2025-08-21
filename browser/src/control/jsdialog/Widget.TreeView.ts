ff49f19ecd979d15888a9e0f3205267eb968bd26
NickWingate <nick.wingate@collabora.com>
2025-08-21 14:01:50 +0100

    QuickFind: Style page dividers differently than results
    Signed-off-by: NickWingate <nick.wingate@collabora.com>
    Change-Id: Iced411634dc06adcc82d7378867fd68572517f29

--- browser/src/control/jsdialog/Widget.TreeView.ts
+++ browser/src/control/jsdialog/Widget.TreeView.ts
@@ -501,6 +501,9 @@ class TreeViewControl {
 		index: any,
 		builder: JSBuilder,
 	) {
+		const PAGE_ENTRY_PREFIX = '-$#~';
+		const PAGE_ENTRY_SUFFIX = '~#$-';
+
 		const text =
 			builder._cleanText(entry.columns[index].text) ||
 			builder._cleanText(entry.text);
@@ -529,7 +532,19 @@ class TreeViewControl {
 			img.alt = text;
 		} else {
 			let cell;
-			if (treeViewData.highlightTerm !== undefined) {
+			if (this.isPageDivider(entry, PAGE_ENTRY_PREFIX, PAGE_ENTRY_SUFFIX)) {
+				cell = L.DomUtil.create(
+					'span',
+					builder.options.cssClass +
+						` ui-treeview-cell-text-content page-divider`,
+					parent,
+				);
+				cell.innerText = this.getPageEntryText(
+					entry.text,
+					PAGE_ENTRY_PREFIX,
+					PAGE_ENTRY_SUFFIX,
+				);
+			} else if (treeViewData.highlightTerm !== undefined) {
 				cell = this.createHighlightedCell(
 					parent,
 					entry,
@@ -611,6 +626,30 @@ class TreeViewControl {
 		}
 
 		return mainSpan;
+	}
+
+	isPageDivider(
+		entry: TreeEntryJSON,
+		pageEntryPrefix: string,
+		pageEntrySuffix: string,
+	): boolean {
+		// Matches page divider prefix and suffix: -$#~ PAGE ~#$- as set in core: QuickFindPanel::FillSearchFindsList() (QuickFindPanel.cxx)
+		return (
+			entry.text &&
+			entry.text.startsWith(pageEntryPrefix) &&
+			entry.text.endsWith(pageEntrySuffix)
+		);
+	}
+
+	getPageEntryText(
+		text: string,
+		pageEntryPrefix: string,
+		pageEntrySuffix: string,
+	): string {
+		return text.substring(
+			pageEntryPrefix.length,
+			text.length - pageEntrySuffix.length,
+		);
 	}
 
 	caseInsensitiveSplit(text: string, delimeter: string) {
