fee98b4db7f64b4e34e4c2b9e91a5abdfc3a4789
Stephan Bergmann <stephan.bergmann@collabora.com>
2025-08-21 13:37:24 +0200

    Enable logging for the Wasm build
    ...redirecting output to the browser console instead of to the underlying Poco
    facilities, and hard-coding a somewhat random "information" log level for now
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Change-Id: Idcb2da6b6128cd20fcce49e96a33768d1b0203cc

--- wsd/LOOLWSD.cpp
+++ wsd/LOOLWSD.cpp
@@ -3820,6 +3820,12 @@ int LOOLWSD::innerMain()
     {
         LOG_DBG("No remote_font_config");
     }
+
+#elif defined __EMSCRIPTEN__
+
+    // Hard-code a somewhat random log level:
+    Log::setLevel("information");
+
 #endif
 
     // URI with /contents are public and we don't need to anonymize them.
