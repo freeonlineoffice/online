93f365824058077da9169cd23779d51d0773cd75
Henry Castro <hcastro@collabora.com>
2025-08-27 09:09:32 -0400

    slideshow: prefix members of main canvas of SlideShowHandler class
    Change-Id: I8b2f18c0eae917fb990167a5029e5923e80e29e2
    Signed-off-by: Henry Castro <hcastro@collabora.com>

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -506,7 +506,7 @@ class SlideShowPresenter {
 				.bind(this._slideShowNavigator),
 		);
 
-		this._slideShowHandler.getContext().aCanvas = canvas;
+		this._slideShowHandler.getContext()._canvas = canvas;
 
 		try {
 			this._slideRenderer = new SlideRendererGl(canvas);
