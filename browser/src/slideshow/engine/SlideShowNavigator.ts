2fbca9e997b1dac5686f2189e27e4629d293bc44
Pranam Lashkari <lpranam@collabora.com>
2025-09-18 16:07:47 +0200

    follow me slideshow: fix move in reverse direction
    problem:
    when follower unfollows and meanwhile leader has moved in
    reverse direction when follower rejoins the presentation
    he may end up on the incorrect position because by going to previous
    slide no effects were played(being on last effect already)
    which means effect number 0. which results into rejoining
    from the beginning of the slide
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Ib7323cf16964169bfde2db8acafb6d26d5e3b2a2

--- browser/src/slideshow/engine/SlideShowNavigator.ts
+++ browser/src/slideshow/engine/SlideShowNavigator.ts
@@ -219,6 +219,7 @@ class SlideShowNavigator {
 		this.isRewindingToPrevSlide = true;
 		this.displaySlide(prevSlide, true);
 		this.isRewindingToPrevSlide = false;
+		this.presenter.sendSlideShowFollowMessage('skipalleffect');
 	}
 
 	setLeaderSlide(info: any) {
@@ -236,7 +237,7 @@ class SlideShowNavigator {
 			this.slideShowHandler.rewindAllEffects();
 		else this.displaySlide(this.currentLeaderSlide, true);
 		for (let i = 0; i <= this.currentLeaderEffect; i++)
-			this.slideShowHandler.skipNextEffect();
+			if (!this.slideShowHandler.skipNextEffect()) break;
 	}
 
 	displaySlide(nNewSlide: number, bSkipTransition: boolean) {
