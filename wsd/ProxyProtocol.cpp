1da31f0edb56967bfea11c61abdee25698cd15d3
Michael Meeks <michael.meeks@collabora.com>
2025-09-26 17:29:40 +0100

    Split the sending path from receiving so we don't close while reading.
    remove the 'wait' path - since it's unused by the JS (sadly).
    
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: Icc795c1880c807521cb65535e27b1c7ea9cc02e4

--- wsd/ProxyProtocol.cpp
+++ wsd/ProxyProtocol.cpp
@@ -228,6 +228,8 @@ void ProxyProtocolHandler::handleRequest(const std::shared_ptr<StreamSocket> &st
         switch (result)
         {
         case ParseStatus::SUCCESS:
+            sendAndClose(streamSocket);
+            break;
         case ParseStatus::AGAIN:
             break;
         case ParseStatus::PROTOCOL_ERROR:
@@ -240,7 +242,10 @@ void ProxyProtocolHandler::handleRequest(const std::shared_ptr<StreamSocket> &st
         }
         };
     }
+}
 
+void ProxyProtocolHandler::sendAndClose(const std::shared_ptr<StreamSocket> &streamSocket)
+{
     bool sentMsg = flushQueueTo(streamSocket);
     // FIXME: we should really restore a blocking 'wait' message
     if (!sentMsg)
@@ -275,6 +280,8 @@ void ProxyProtocolHandler::handleIncomingMessage(SocketDisposition &disposition)
         break;
     }
     case ParseStatus::SUCCESS:
+        sendAndClose(streamSocket);
+        break;
     case ParseStatus::AGAIN:
         break;
     };
