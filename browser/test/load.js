a6d4fb1b85f3af02141288870a73ce9d35126ab5
Michael Meeks <michael.meeks@collabora.com>
2025-10-02 12:21:56 +0100

    spdx: improve machine and human readability of headers.
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Michael Meeks <michael.meeks@collabora.com>
    Change-Id: I2f991921096113d04eeb781363f2763d911cc0f3

--- browser/test/load.js
+++ browser/test/load.js
@@ -1,5 +1,12 @@
-var vm = require('vm');
-var fs = require('fs');
+/*
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+var vm = require("vm");
+var fs = require("fs");
 var tmp = require('tmp');
 const WebSocket = require('ws');
 
