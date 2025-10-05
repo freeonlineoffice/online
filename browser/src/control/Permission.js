eb244b957ab69846b9f75f887dd756eabdebaad5
Pranam Lashkari <lpranam@collabora.com>
2025-10-06 02:24:13 +0530

    UI: enter edit mode in write protected file with edit button (mobile)
    by default documents are opened in read only mode in mobile
    to prevent accidental editing.
    This read only mode is different from when file is actually
    set with permission read-only(in desktop Edit -> Edit mode)
    
    Currently online does not have any button equivalent to the desktop
    to enter the edit mode, neither in desktop browser nor in mobile.
    
    Without this patch if file is read only and user presses the pencil button,
    UI changes to edit mode but file still can not be edited because of
    restricted permissions.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Id2f9b787e4a6f6456c4d192987d2650a3b2b27d0

--- browser/src/control/Permission.js
+++ browser/src/control/Permission.js
@@ -266,7 +266,10 @@ window.L.Map.include({
 			this._textInput.setSwitchedToEditMode();
 		}
 
-		app.events.fire('updatepermission', { perm: perm });
+		if (app.map['stateChangeHandler'].getItemValue('EditDoc') === 'false')
+			app.map.sendUnoCommand('.uno:EditDoc?Editable:bool=true');
+
+		app.events.fire('updatepermission', {perm : perm});
 
 		if (this._docLayer._docType === 'text') {
 			this.setZoom(10);
