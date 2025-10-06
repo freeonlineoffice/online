a366286a9c7a0a986046623a153de8f7833b38d3
Gökay Şatır <gokaysatir@gmail.com>
2025-10-06 14:22:31 +0300

    Remove hardcoded id and add "ignoreFocus" property to TreeViewControl.
    The intention is to keep focus on current HTML element when selecting a tree view item.
    
    Signed-off-by: Gökay Şatır <gokaysatir@gmail.com>
    Change-Id: Ie9bf6922e80060da0a206661b0be1844cf370cff

--- browser/src/control/jsdialog/Widget.TreeView.ts
+++ browser/src/control/jsdialog/Widget.TreeView.ts
@@ -79,6 +79,7 @@ class TreeViewControl {
 	readonly PAGE_ENTRY_PREFIX = '-$#~';
 	readonly PAGE_ENTRY_SUFFIX = '~#$-';
 	readonly PAGE_DIVIDER_ROW_CLASS = 'page-divider-row';
+	_ignoreFocus: boolean = false;
 
 	constructor(data: TreeWidgetJSON, builder: JSBuilder) {
 		this._container = window.L.DomUtil.create(
@@ -89,6 +90,7 @@ class TreeViewControl {
 		this._rows = new Map<string, HTMLElement>();
 		if (data.labelledBy)
 			this._container.setAttribute('aria-labelledby', data.labelledBy);
+		if (data.ignoreFocus !== undefined) this._ignoreFocus = data.ignoreFocus;
 	}
 
 	get Container() {
@@ -1025,7 +1027,7 @@ class TreeViewControl {
 		window.L.DomUtil.addClass(span, 'selected');
 		span.setAttribute('aria-selected', 'true');
 		span.tabIndex = 0;
-		if (this._container.id !== 'searchfinds') span.focus();
+		if (!this._ignoreFocus) span.focus();
 
 		if (checkbox) checkbox.removeAttribute('tabindex');
 	}
