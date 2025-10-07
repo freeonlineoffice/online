3ba03c64b32a4541a8dc059cec2ce06a7a52c580
Pranam Lashkari <lpranam@collabora.com>
2025-10-07 17:37:14 +0530

    followme slideshow: fix incorrect following status
    regression from: de449ef0a242b4313eeea4b5f48ccb87babc0360
    deleted a line by mistake
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Pranam Lashkari <lpranam@collabora.com>
    Change-Id: Ifadd4ebe99d22a34944c1bb3b3d018215a458455

--- browser/src/slideshow/SlideShowPresenter.ts
+++ browser/src/slideshow/SlideShowPresenter.ts
@@ -220,6 +220,7 @@ class SlideShowPresenter {
 		this.setFollower(true);
 		switch (info.type) {
 			case 'newfollowmepresentation':
+				this.setFollowing(true);
 				this._map.uiManager.showButton(
 					'slide-presentation-follow-me',
 					false,
