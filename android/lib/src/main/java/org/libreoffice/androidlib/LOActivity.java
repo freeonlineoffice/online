ff3428d03deb085269e5745f910c62980d09d09d
Skyler Grey <skyler.grey@collabora.com>
2025-10-09 15:41:17 +0000

    chore(android): bump dependencies
    I'm in the process of enabling edge-to-edge mode on Android. To do that,
    I'm upgrading Android dependencies and then adding margin/padding for
    anything which doesn't apply these automatically. This is the upgrading
    dependencies step.
    
    A nontrivial change here is removing the broadcast manager, which has
    been deprecated for a long time. It's suggested to use a different
    solution (most people seem to use LiveData), however it turns out that
    we were actually doing nothing with the broadcasts anyway, save for
    logging them - and indeed there was a comment condemning the listener! I
    think it's easy and safe enough to just remove it without working to add
    its replacement...
    
    I've also had to change the back pressed handler to be via the new
    callback interface rather than the old override one, since as new lints
    were rejecting the code until I did
    
    Signed-off-by: Skyler Grey <skyler.grey@collabora.com>
    Change-Id: I6a6a6964179cddc0a25c55e794e0c7ef927e59ac

--- android/lib/src/main/java/org/libreoffice/androidlib/LOActivity.java
+++ android/lib/src/main/java/org/libreoffice/androidlib/LOActivity.java
@@ -79,13 +79,13 @@ import java.util.Objects;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -255,6 +255,28 @@ public class LOActivity extends AppCompatActivity {
         else
             this.rateAppController = null;
         this.mActivity = this;
+
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (!documentLoaded) {
+                    finishAndRemoveTask();
+                    return;
+                }
+
+                if (mMobileWizardVisible) {
+                    // just return one level up in the mobile-wizard (or close it)
+                    callFakeWebsocketOnMessage("mobile: mobilewizardback");
+                    return;
+                } else if (mIsEditModeActive) {
+                    callFakeWebsocketOnMessage("mobile: readonlymode");
+                    return;
+                }
+
+                finishWithProgress();
+            }
+        });
+
         init();
     }
 
@@ -815,25 +837,6 @@ public class LOActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onBackPressed() {
-        if (!documentLoaded) {
-            finishAndRemoveTask();
-            return;
-        }
-
-        if (mMobileWizardVisible) {
-            // just return one level up in the mobile-wizard (or close it)
-            callFakeWebsocketOnMessage("mobile: mobilewizardback");
-            return;
-        } else if (mIsEditModeActive) {
-            callFakeWebsocketOnMessage("mobile: readonlymode");
-            return;
-        }
-
-        finishWithProgress();
-    }
-
     private void loadDocument() {
         mProgressDialog.determinate(R.string.loading);
 
@@ -1341,7 +1344,6 @@ public class LOActivity extends AppCompatActivity {
         Intent intent = new Intent(LO_ACTIVITY_BROADCAST);
         intent.putExtra(LO_ACTION_EVENT, event);
         intent.putExtra(LO_ACTION_DATA, data);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 
     public native void saveAs(String fileUri, String format, String options);
