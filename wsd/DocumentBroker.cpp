dd2c33f18ef16f7b7cbbe9fa8aea04c3b1447522
Ashod Nakashian <ashod.nakashian@collabora.co.uk>
2025-10-17 06:41:02 -0400

    wsd: move pass-through argument to setupTransfer
    Change-Id: I62980ff266bd2126ae229f55c5b42169ee3e4097
    Signed-off-by: Ashod Nakashian <ashod.nakashian@collabora.co.uk>

--- wsd/DocumentBroker.cpp
+++ wsd/DocumentBroker.cpp
@@ -61,6 +61,7 @@
 #include <string>
 #include <sys/types.h>
 #include <sysexits.h>
+#include <utility>
 
 using namespace std::literals;
 using namespace LOOLProtocol;
@@ -259,9 +260,9 @@ void DocumentBroker::setupTransfer(SocketDisposition &disposition,
 }
 
 void DocumentBroker::setupTransfer(SocketPoll& from, const std::weak_ptr<StreamSocket>& socket,
-                                   const SocketDisposition::MoveFunction& transferFn)
+                                   SocketDisposition::MoveFunction transferFn)
 {
-    from.transferSocketTo(socket, getPoll(), transferFn, nullptr);
+    from.transferSocketTo(socket, getPoll(), std::move(transferFn), nullptr);
 }
 
 static std::chrono::seconds getLimitLoadSecs()
