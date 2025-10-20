f64fc2e0658f231f36ba741e59d5438a74d226bd
Szymon Kłos <szymon.klos@collabora.com>
2025-10-20 16:33:05 +0000

    jsdialog: fix default button action
    - we introduced the default button support in
      commit e36eabd3588b7759d739a1d9926cb5ba00a276f6
      jsdialog: handle default button
    
    - but button got additional wrapper due to
      commit e02e1df76d8379e4da7e7c87f10c594bc558d6d2
      Introduce Freemium options
    
    what made the click event target wrong (wrapper, not a button)
    
    This fixes behavior of "enter" pressed when focused inside edit field
    in Calc rename input modal
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Szymon Kłos <szymon.klos@collabora.com>
    Change-Id: I100bb82d8bea61ce0c770b282091ab2e0d881b00

--- browser/src/control/Control.JSDialog.js
+++ browser/src/control/Control.JSDialog.js
@@ -351,10 +351,9 @@ window.L.Control.JSDialog = window.L.Control.extend({
 		defaultButton.style.display = 'none';
 		defaultButton.onclick = function () {
 			if (instance.defaultButtonId) {
-				var button = instance.form.querySelector(
-					'#' + instance.defaultButtonId,
-				);
-				if (button) button.click();
+				const button = instance.form.querySelector('#' + instance.defaultButtonId + ' button');
+				if (button)
+					button.click();
 			}
 		};
 
