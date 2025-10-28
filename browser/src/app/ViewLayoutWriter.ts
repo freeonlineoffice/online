1eacf7bf2a062ac649b262b57f56fdef511313e5
Sahil Gautam <sahil.gautam@collabora.com>
2025-10-28 09:03:43 +0530

    code cleanup
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Sahil Gautam <sahil.gautam@collabora.com>
    Change-Id: I17fe4d98c6ebb13d8c8b9d1f47dbfbc8fb59ecce

--- browser/src/app/ViewLayoutWriter.ts
+++ browser/src/app/ViewLayoutWriter.ts
@@ -7,11 +7,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-interface DocumentSpacingInfo {
-	spaceOnDocumentLeft: number;
-	spaceOnDocumentRight: number;
+type DocumentSpacingInfo = {
+	documentMarginsWidth: number;
 	commentSectionWidth: number;
-}
+};
 
 class ViewLayoutWriter extends ViewLayoutBase {
 	public readonly type: string = 'ViewLayoutWriter';
@@ -38,30 +37,23 @@ class ViewLayoutWriter extends ViewLayoutBase {
 		) as lool.CommentSection;
 
 		return {
-			spaceOnDocumentRight: commentSection.calculateAvailableSpace(),
-			commentSectionWidth:
-				commentSection.sectionProperties.commentWidth,
-			spaceOnDocumentLeft: commentSection.calculateAvailableSpace(),
+			documentMarginsWidth: commentSection.calculateAvailableSpace(),
+			commentSectionWidth: commentSection.sectionProperties.commentWidth,
 		} as DocumentSpacingInfo;
 	}
 
 	public documentCanMoveLeft() {
 		const spacingInfo = this.getCommentAndDocumentSpacingInfo();
 		return (
-			spacingInfo.spaceOnDocumentRight <
-				spacingInfo.commentSectionWidth &&
-			spacingInfo.commentSectionWidth -
-				spacingInfo.spaceOnDocumentRight <
-				spacingInfo.spaceOnDocumentLeft
+			spacingInfo.documentMarginsWidth < spacingInfo.commentSectionWidth &&
+			spacingInfo.commentSectionWidth - spacingInfo.documentMarginsWidth <
+				spacingInfo.documentMarginsWidth
 		);
 	}
 
 	private documentMoveLeftByOffset(): number {
 		const spacingInfo = this.getCommentAndDocumentSpacingInfo();
-		return (
-			spacingInfo.commentSectionWidth -
-			spacingInfo.spaceOnDocumentRight
-		);
+		return spacingInfo.commentSectionWidth - spacingInfo.documentMarginsWidth;
 	}
 
 	private adjustDocumentMarginsForComments() {
