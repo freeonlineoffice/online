ce75ad2793201150389c6b8967681fd715342c21
Gülşah Köse <gulsah.kose@collabora.com>
2025-11-05 15:00:24 +0300

    Handle image/png type on paste from external source
    Copy an image from desktop app and paste into the document via context
    menu or Paste button at the top toolbar was not working.
    
    So we improved the clipboard read code with this patch.
    
    Adapt patch to LibreOffice Online.
    
    Signed-off-by: Gülşah Köse <gulsah.kose@collabora.com>
    Change-Id: Ie29644d729e9110cadf3119b32b9364ca300baba

--- browser/src/map/Clipboard.js
+++ browser/src/map/Clipboard.js
@@ -1261,11 +1261,16 @@ window.L.Clipboard = window.L.Class.extend({
 				);
 				return;
 			}
-			this._navigatorClipboardGetTypeCallback(
-				clipboardContent,
-				blob,
-				'text/plain',
-			);
+			this._navigatorClipboardGetTypeCallback(clipboardContent, blob, 'text/plain');
+		} else if (clipboardContent.types.includes('image/png')) {
+			let blob;
+			try {
+				blob = await clipboardContent.getType('image/png');
+			} catch (error) {
+				window.app.console.log('clipboardContent.getType(image/png) failed: ' + error.message);
+				return;
+			}
+			this._navigatorClipboardGetTypeCallback(clipboardContent, blob, 'image/png');
 		} else {
 			window.app.console.log(
 				'navigator.clipboard has no text/html or text/plain',
